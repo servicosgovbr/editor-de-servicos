1d1b95cc2abc90f6068f4d987c3cdcee4b60bc63
Carlos Villela <cvillela@thoughtworks.com>
2015-06-15 17:57:00 -0300

    Primeiro teste usando Selenium - #8

---
+++ src/test/java/br/gov/servicos/editor/frontend/UITest.java
@@ -0,0 +1,50 @@
+package br.gov.servicos.editor.frontend;
+
+import br.gov.servicos.editor.Main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class UITest {
+
+    WebDriver driver;
+    boolean acceptNextAlert = true;
+    StringBuffer verificationErrors = new StringBuffer();
+
+    @Value("${local.server.port}")
+    int port;
+
+    String baseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        baseUrl = "http://localhost:" + port;
+
+        driver = new HtmlUnitDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void deveLogarComoUsuarioSimples() throws Exception {
+        driver.get(baseUrl + "/exemplo");
+        assertThat(driver.getTitle(), is("Editor de Serviços - Acessar o editor de serviços"));
+    }
+
+
+}
