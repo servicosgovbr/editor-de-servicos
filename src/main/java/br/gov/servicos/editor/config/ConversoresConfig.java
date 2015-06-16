1ac3d17b5be81febd317acb707308ad5be395311
Carlos Villela <cvillela@thoughtworks.com>
2015-06-16 17:48:55 -0300

    Mapeando Servico para XML - #8

--- src/main/java/br/gov/servicos/editor/config/ConversoresConfig.java
+++ src/main/java/br/gov/servicos/editor/config/ConversoresConfig.java
@@ -1,13 +1,15 @@
 package br.gov.servicos.editor.config;
 
 import br.gov.servicos.editor.servicos.Orgao;
+import com.github.slugify.Slugify;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.Formatter;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Locale;
 
@@ -16,6 +18,11 @@ public class ConversoresConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     ConteudoDeReferencia referencia;
+
+    @Bean
+    Slugify slugify() throws IOException {
+        return new Slugify(true);
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
--- src/main/java/br/gov/servicos/editor/frontend/IndexController.java
+++ src/main/java/br/gov/servicos/editor/frontend/IndexController.java
@@ -1,6 +1,10 @@
 package br.gov.servicos.editor.frontend;
 
 import br.gov.servicos.editor.servicos.Servico;
+import com.github.slugify.Slugify;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,10 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 class IndexController {
+
+    @Autowired
+    Slugify slugify;
 
     @RequestMapping("/")
     ModelAndView index() {
@@ -20,7 +30,80 @@ class IndexController {
 
     @RequestMapping(value = "/salvar", method = POST)
     RedirectView salvar(@ModelAttribute("servico") Servico servico, BindingResult result) {
-        System.out.println("servico = " + servico);
+
+        Document document = new Document("");
+        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+
+        document.children().remove();
+        Element root = document.appendElement("servico")
+                .appendElement("nome").text(servico.getNome()).parent()
+                .appendElement("nomes-populares").text(servico.getNomesPopulares()).parent()
+                .appendElement("descricao").text(servico.getDescricao()).parent()
+                .appendElement("palavras-chave").text(servico.getPalavrasChave()).parent();
+
+        Element areas = root.appendElement("areas-de-interesse");
+        Optional.ofNullable(servico.getAreasDeInteresse()).orElse(emptyList())
+                .stream()
+                .forEach(a -> areas.appendElement("area-de-interesse")
+                        .appendElement("id").text(slugify.slugify(a)).parent()
+                        .appendElement("nome").text(a).parent());
+
+        Element segmentos = root.appendElement("segmentos-da-sociedade");
+        Optional.ofNullable(servico.getSegmentosDaSociedade()).orElse(emptyList())
+                .stream()
+                .forEach(s -> segmentos.appendElement("segmento-da-sociedade")
+                        .appendElement("id").text(slugify.slugify(s)).parent()
+                        .appendElement("nome").text(s).parent());
+
+        Element eventos = root.appendElement("eventos-da-linha-da-vida");
+        Optional.ofNullable(servico.getEventosDaLinhaDaVida()).orElse(emptyList())
+                .stream()
+                .forEach(e -> eventos.appendElement("evento-da-linha-da-vida")
+                        .appendElement("id").text(slugify.slugify(e)).parent()
+                        .appendElement("nome").text(e).parent());
+
+        Element legislacao = root.appendElement("legislacao-relacionada");
+        Optional.ofNullable(servico.getLegislacoes()).orElse(emptyList())
+                .stream()
+                .forEach(link -> legislacao.appendElement("link").attr("href", link));
+
+        Element solicitantes = root.appendElement("solicitantes");
+        Optional.ofNullable(servico.getSolicitantes()).orElse(emptyList())
+                .stream()
+                .forEach(s -> solicitantes.appendElement("solicitante").text(s));
+
+        Optional.ofNullable(servico.getTempoEstimado()).ifPresent(t ->
+                        root.appendElement("tempo-total-estimado")
+                                .attr("tipo", t.getTipo())
+                                .appendElement("minimo").attr("tipo", t.getTipoMinimo()).text(t.getMinimo()).parent()
+                                .appendElement("maximo").attr("tipo", t.getTipoMaximo()).text(t.getMaximo()).parent()
+                                .appendElement("excecoes").text(t.getExcecoes())
+        );
+
+        Element etapas = root.appendElement("etapas");
+        Optional.ofNullable(servico.getEtapas()).orElse(emptyList())
+                .stream()
+                .forEach(e -> etapas.appendElement("etapa")
+                                .appendElement("titulo").text(e.getTitulo()).parent()
+                                .appendElement("descricao").text(e.getDescricao()).parent()
+                                .appendElement("documentos").parent()
+                                .appendElement("custos").attr("excecoes", e.getCustoTemExcecoes().toString()).parent()
+                                .appendElement("canais-de-prestacao").parent()
+                );
+
+        Optional.ofNullable(servico.getOrgao()).ifPresent(o ->
+                root.appendElement("orgao")
+                        .appendElement("id").text(o.getId()).parent()
+                        .appendElement("nome").text(o.getNome()));
+
+        Optional.ofNullable(servico.getGratuito()).ifPresent(o ->
+                root.appendElement("gratuito").text(o.toString()));
+
+        Optional.ofNullable(servico.getSituacao()).ifPresent(s ->
+                root.appendElement("situacao").text(s));
+
+        System.out.println("document = " + document);
+
         return new RedirectView("/");
     }
 
