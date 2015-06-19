ef5aeefc2a93408b0511fa19e192118db4f76fe0
Carlos Villela <cvillela@thoughtworks.com>
2015-06-18 23:35:47 -0300

    Extrai métodos - #8

--- src/main/java/br/gov/servicos/editor/frontend/ExportadorServicoV2.java
+++ src/main/java/br/gov/servicos/editor/frontend/ExportadorServicoV2.java
@@ -40,55 +40,38 @@ public class ExportadorServicoV2 {
                 .appendElement("descricao").html(servico.getDescricao()).parent()
                 .appendElement("palavras-chave").text(servico.getPalavrasChave()).parent();
 
-        Element areas = root.appendElement("areas-de-interesse");
-        Optional.ofNullable(servico.getAreasDeInteresse()).orElse(emptyList())
-                .stream()
-                .forEach(a -> areas.appendElement("area-de-interesse")
-                        .appendElement("id").text(slugify.slugify(a)).parent()
-                        .appendElement("nome").text(a).parent());
+        areasDeInteresse(servico, root);
+        segmentosDaSociedade(servico, root);
+        eventosDaLinhaDaVida(servico, root);
+        legislacaoRelacionada(servico, root);
+        solicitantes(servico, root);
+        tempoTotalEstimado(servico, root);
+        etapas(servico, root);
+        orgao(servico, root);
+        gratuito(servico, root);
+        situacaoDoServico(servico, root);
 
-        Element segmentos = root.appendElement("segmentos-da-sociedade");
-        Optional.ofNullable(servico.getSegmentosDaSociedade()).orElse(emptyList())
-                .stream()
-                .forEach(s -> segmentos.appendElement("segmento-da-sociedade")
-                        .appendElement("id").text(slugify.slugify(s)).parent()
-                        .appendElement("nome").text(s).parent());
+        return document;
+    }
 
-        Element eventos = root.appendElement("eventos-da-linha-da-vida");
-        Optional.ofNullable(servico.getEventosDaLinhaDaVida()).orElse(emptyList())
-                .stream()
-                .forEach(e -> eventos.appendElement("evento-da-linha-da-vida")
-                        .appendElement("id").text(slugify.slugify(e)).parent()
-                        .appendElement("nome").text(e).parent());
+    private void situacaoDoServico(Servico servico, Element root) {
+        Optional.ofNullable(servico.getSituacao()).ifPresent(s ->
+                root.appendElement("situacao").text(s));
+    }
 
-        Element legislacao = root.appendElement("legislacao-relacionada");
-        Optional.ofNullable(servico.getLegislacoes()).orElse(emptyList())
-                .stream()
-                .forEach(link -> legislacao.appendElement("link").attr("href", link));
+    private void gratuito(Servico servico, Element root) {
+        Optional.ofNullable(servico.getGratuito()).ifPresent(o ->
+                root.appendElement("gratuito").text(o.toString()));
+    }
 
-        Element solicitantes = root.appendElement("solicitantes");
-        Optional.ofNullable(servico.getSolicitantes()).orElse(emptyList())
-                .stream()
-                .forEach(s -> solicitantes.appendElement("solicitante").text(s));
+    private void orgao(Servico servico, Element root) {
+        Optional.ofNullable(servico.getOrgao()).ifPresent(o ->
+                root.appendElement("orgao")
+                        .appendElement("id").text(o.getId()).parent()
+                        .appendElement("nome").text(o.getNome()));
+    }
 
-        Optional.ofNullable(servico.getTempoEstimado()).ifPresent(t -> {
-                    Element tempo = root.appendElement("tempo-total-estimado");
-
-                    Optional.ofNullable(t.getTipo()).ifPresent(tipo -> {
-                        if (tipo.equals("entre")) {
-                            tempo.attr("tipo", tipo)
-                                    .appendElement("minimo").attr("tipo", t.getEntreTipoMinimo()).text(t.getEntreMinimo()).parent()
-                                    .appendElement("maximo").attr("tipo", t.getEntreTipoMaximo()).text(t.getEntreMaximo()).parent()
-                                    .appendElement("excecoes").text(t.getExcecoes());
-                        } else if (tipo.equals("até")) {
-                            tempo.attr("tipo", tipo)
-                                    .appendElement("maximo").attr("tipo", t.getAteTipoMaximo()).text(t.getAteMaximo()).parent()
-                                    .appendElement("excecoes").text(t.getExcecoes());
-                        }
-                    });
-                }
-        );
-
+    private void etapas(Servico servico, Element root) {
         Element etapas = root.appendElement("etapas");
         Optional.ofNullable(servico.getEtapas()).orElse(emptyList())
                 .stream()
@@ -112,18 +95,66 @@ public class ExportadorServicoV2 {
                                             .appendElement("descricao").text(c.getDescricao()));
                         }
                 );
+    }
 
-        Optional.ofNullable(servico.getOrgao()).ifPresent(o ->
-                root.appendElement("orgao")
-                        .appendElement("id").text(o.getId()).parent()
-                        .appendElement("nome").text(o.getNome()));
+    private void tempoTotalEstimado(Servico servico, Element root) {
+        Optional.ofNullable(servico.getTempoEstimado()).ifPresent(t -> {
+                    Element tempo = root.appendElement("tempo-total-estimado");
 
-        Optional.ofNullable(servico.getGratuito()).ifPresent(o ->
-                root.appendElement("gratuito").text(o.toString()));
+                    Optional.ofNullable(t.getTipo()).ifPresent(tipo -> {
+                        if (tipo.equals("entre")) {
+                            tempo.attr("tipo", tipo)
+                                    .appendElement("minimo").attr("tipo", t.getEntreTipoMinimo()).text(t.getEntreMinimo()).parent()
+                                    .appendElement("maximo").attr("tipo", t.getEntreTipoMaximo()).text(t.getEntreMaximo()).parent()
+                                    .appendElement("excecoes").text(t.getExcecoes());
+                        } else if (tipo.equals("até")) {
+                            tempo.attr("tipo", tipo)
+                                    .appendElement("maximo").attr("tipo", t.getAteTipoMaximo()).text(t.getAteMaximo()).parent()
+                                    .appendElement("excecoes").text(t.getExcecoes());
+                        }
+                    });
+                }
+        );
+    }
 
-        Optional.ofNullable(servico.getSituacao()).ifPresent(s ->
-                root.appendElement("situacao").text(s));
+    private void solicitantes(Servico servico, Element root) {
+        Element solicitantes = root.appendElement("solicitantes");
+        Optional.ofNullable(servico.getSolicitantes()).orElse(emptyList())
+                .stream()
+                .forEach(s -> solicitantes.appendElement("solicitante").text(s));
+    }
 
-        return document;
+    private void legislacaoRelacionada(Servico servico, Element root) {
+        Element legislacao = root.appendElement("legislacao-relacionada");
+        Optional.ofNullable(servico.getLegislacoes()).orElse(emptyList())
+                .stream()
+                .forEach(link -> legislacao.appendElement("link").attr("href", link));
+    }
+
+    private void eventosDaLinhaDaVida(Servico servico, Element root) {
+        Element eventos = root.appendElement("eventos-da-linha-da-vida");
+        Optional.ofNullable(servico.getEventosDaLinhaDaVida()).orElse(emptyList())
+                .stream()
+                .forEach(e -> eventos.appendElement("evento-da-linha-da-vida")
+                        .appendElement("id").text(slugify.slugify(e)).parent()
+                        .appendElement("nome").text(e).parent());
+    }
+
+    private void segmentosDaSociedade(Servico servico, Element root) {
+        Element segmentos = root.appendElement("segmentos-da-sociedade");
+        Optional.ofNullable(servico.getSegmentosDaSociedade()).orElse(emptyList())
+                .stream()
+                .forEach(s -> segmentos.appendElement("segmento-da-sociedade")
+                        .appendElement("id").text(slugify.slugify(s)).parent()
+                        .appendElement("nome").text(s).parent());
+    }
+
+    private void areasDeInteresse(Servico servico, Element root) {
+        Element areas = root.appendElement("areas-de-interesse");
+        Optional.ofNullable(servico.getAreasDeInteresse()).orElse(emptyList())
+                .stream()
+                .forEach(a -> areas.appendElement("area-de-interesse")
+                        .appendElement("id").text(slugify.slugify(a)).parent()
+                        .appendElement("nome").text(a).parent());
     }
 }
