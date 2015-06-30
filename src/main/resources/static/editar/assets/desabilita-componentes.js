8479dcc1763bc8eede1804a15a881327d6482dc6
Jean Kirchner <jean.kirchner@gmail.com>
2015-06-30 19:01:44 -0300

    Correção de testes - #79

--- src/main/resources/static/editar/assets/desabilita-componentes.js
+++ src/main/resources/static/editar/assets/desabilita-componentes.js
@@ -5,7 +5,8 @@ jQuery(function($) {
     }
 
     $('#principal > form').on('submit', function(e) {
-        $('fieldset, input, button, textarea').prop('disabled', true)
+        $('fieldset, input, textarea').prop('readonly', true)
+        $('input[type=submit], button').prop('disabled', true)
         $('a').prop('href', 'javascript:')
 
         return true;
