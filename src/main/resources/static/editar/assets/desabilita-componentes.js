457ba17ff56a096b23cda4bf3061febe7b988b02
Jean Kirchner <jean.kirchner@gmail.com>
2015-07-02 22:22:52 -0300

    Apenas deixa os componentes readonly, quando desabilitados não são submetidos pelo formularário - #80

--- src/main/resources/static/editar/assets/desabilita-componentes.js
+++ src/main/resources/static/editar/assets/desabilita-componentes.js
@@ -1,14 +1,6 @@
 jQuery(function($) {
-
-    function desabilitaComponente(c) {
-        $(c).prop('disabled', true);
-    }
-
     $('#principal > form').on('submit', function(e) {
         $('fieldset, input, textarea').prop('readonly', true)
-        $('input[type=submit], button').prop('disabled', true)
-        $('a').prop('href', 'javascript:')
-
         return true;
     })
 });
