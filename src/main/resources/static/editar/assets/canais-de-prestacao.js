5cd7729af342187656138013459b682ca90cb5b9
Carlos Villela <cvillela@thoughtworks.com>
2015-07-14 19:55:56 -0300

    Agrupa canais de prestação, corrige comportamento - resolve #130

--- src/main/resources/static/editar/assets/canais-de-prestacao.js
+++ src/main/resources/static/editar/assets/canais-de-prestacao.js
@@ -1,24 +1,22 @@
 jQuery(function ($) {
     var onCanalDePrestacaoSelect = function(container, option) {
-        option = ("" || option).trim().toLowerCase();
-        var labelDescricao = $(container).find('label.descricao');
-        var inputDescricao = $(container).find('label.descricao > input');
-        var labelReferencia = $(container).find('label.referencia > span');
+        option = (option || "").trim().toLowerCase();
+        var titulo = $(container).find('label.titulo');
+        var referencia = $(container).find('label.referencia > span');
 
         if (option == "web") {
-            labelDescricao.show();
-            labelReferencia.val("Link/URL");
+            titulo.show();
+            referencia.val("Link/URL");
         } else {
-            labelDescricao.hide();
-            inputDescricao.val("");
-            labelReferencia.text("");
+            titulo.hide().find('input').val('');
+            referencia.text("");
         }
     };
 
-    $('.canal-prestacao').each(function() {
-        var container = this;
-        $(container).find('select').change(function() {
-            onCanalDePrestacaoSelect($(container), $(this).val());
+    $('.grupo-canal-prestacao').each(function() {
+        var container = $(this);
+        container.find('select').change(function() {
+            onCanalDePrestacaoSelect(container, $(this).val());
         });
     });
 });
