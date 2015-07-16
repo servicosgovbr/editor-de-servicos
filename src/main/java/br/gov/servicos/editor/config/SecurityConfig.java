0d9d7aeef03925af61bc11e1dc20637acacf1bdc
Carlos Villela <cvillela@thoughtworks.com>
2015-07-16 10:29:28 -0300

    Adiciona usuarios da oficina

--- src/main/java/br/gov/servicos/editor/config/SecurityConfig.java
+++ src/main/java/br/gov/servicos/editor/config/SecurityConfig.java
@@ -32,6 +32,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         String hash = "46022771041f1486dc97b709a9bb35a3601ed9b7cbf82df729fcdf5319d48df0cad832848c6c3e82";
+        String hash2 = "46022771041f14862063fa299c50f2ae429cd0f23dae153762bea01712bd2e6090e275097c355b74";
 
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder)
@@ -44,7 +45,19 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("cvillela@thoughtworks.com").password(hash).roles(PUBLICADOR).and()
                 .withUser("oliviaj@thoughtworks.com").password(hash).roles(PUBLICADOR).and()
                 .withUser("srosa@thoughtworks.com").password(hash).roles(PUBLICADOR).and()
-                .withUser("jkirchne@thoughtworks.com").password(hash).roles(PUBLICADOR)
+                .withUser("jkirchne@thoughtworks.com").password(hash).roles(PUBLICADOR).and()
+                .withUser("joelson.vellozo@planejamento.gov.br").password(hash).roles(PUBLICADOR).and()
+
+                // usuarios da oficina 16/jul
+                .withUser("martins@aneel.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("alysson@aneel.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("monica.ribeiro@mds.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("ariana.souza@mds.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("nicir.chaves@previdencia.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("celia.torres@previdencia.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("matheus.foliveira@mj.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("lucas.fernandes@mj.gov.br").password(hash2).roles(PUBLICADOR).and()
+                .withUser("raquel.sinfronio@mj.gov.br").password(hash2).roles(PUBLICADOR)
         ;
     }
 }
