c5477ff362612fd07ddf92d5dcb376060a2252e7
Carlos Villela <cvillela@thoughtworks.com>
2015-07-22 11:45:29 -0300

    Binding para areas de interesse

--- src/main/resources/static/editar/assets/editor.js
+++ src/main/resources/static/editar/assets/editor.js
@@ -16,6 +16,7 @@ models.Servico = function (data) {
   this.orgao = m.prop(data.orgao || '');
   this.segmentosDaSociedade = m.prop(data.segmentosDaSociedade || []);
   this.eventosDaLinhaDaVida = m.prop(data.eventosDaLinhaDaVida || []);
+  this.areasDeInteresse = m.prop(data.areasDeInteresse || []);
 };
 
 models.TempoTotalEstimado = function(data) {
@@ -346,13 +347,28 @@ var AreasDeInteresse = {
   controller: function(args) {
     this.servico = args.servico;
     this.areasDeInteresse = m.request({ method: 'GET', url: '/editar/api/areas-de-interesse' });
+
+    this.adicionar = function(e) {
+      var area = e.target.value;
+      var areas = this.servico.areasDeInteresse();
+
+      areas = _.without(areas, area);
+      if (e.target.checked) {
+        areas.push(area);
+      }
+      this.servico.areasDeInteresse(areas);
+    }
   },
   view: function(ctrl) {
     return m('', [
       m("h3", "Áreas de interesse"),
       m("", ctrl.areasDeInteresse().map(function(area) {
         return m('label', [
-          m("input[type=checkbox]", { value: area }),
+          m("input[type=checkbox]", {
+            value: area,
+            checked: _.contains(ctrl.servico.areasDeInteresse(), area),
+            onchange: ctrl.adicionar.bind(ctrl)
+          }),
           area
         ]);
       }))
