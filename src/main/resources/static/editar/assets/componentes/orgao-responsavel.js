f4fd4d8f33f8c0a2afd843986be119eb0417c696
Carlos Villela <cvillela@thoughtworks.com>
2015-07-23 18:50:21 -0300

    Renomeia propriedade

--- src/main/resources/static/editar/assets/componentes/orgao-responsavel.js
+++ src/main/resources/static/editar/assets/componentes/orgao-responsavel.js
@@ -2,7 +2,7 @@ var OrgaoResponsavel = {
 
   controller: function(args) {
     this.orgao = args.orgao;
-    this.orgaos = m.request({ method: 'GET', url: '/editar/api/orgaos' });
+    this.todosOrgaos = m.request({ method: 'GET', url: '/editar/api/orgaos' });
   },
 
   view: function(ctrl) {
@@ -10,7 +10,7 @@ var OrgaoResponsavel = {
       m("h3", "Órgão responsável"),
       m("select", {
         onchange: function(e) { ctrl.orgao(e.target.value); }
-      }, [m('option', {value: ''}, 'Selecione...')].concat(ctrl.orgaos().map(function(orgao) {
+      }, [m('option', {value: ''}, 'Selecione...')].concat(ctrl.todosOrgaos().map(function(orgao) {
         return m('option', {
           value: orgao.id,
           selected: ctrl.orgao() === orgao.id
