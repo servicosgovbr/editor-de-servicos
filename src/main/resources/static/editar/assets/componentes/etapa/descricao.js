84ef92a74b1151c67092917f9c8782387547adb5
Carlos Villela <cvillela@thoughtworks.com>
2015-07-23 16:57:56 -0300

    Diminui tamanho do campo de descricao

--- src/main/resources/static/editar/assets/componentes/etapa/descricao.js
+++ src/main/resources/static/editar/assets/componentes/etapa/descricao.js
@@ -10,7 +10,7 @@ etapa.Descricao = {
     return m('.descricao', [
       m('h3', 'Descrição da etapa'),
       m.component(EditorMarkdown, {
-        rows: 10,
+        rows: 5,
         oninput: m.withAttr('value', ctrl.descricao),
         value: ctrl.descricao()
       })
