fe254c832f08c4d63cae669daf57a053083139d8
Carlos Villela <cvillela@thoughtworks.com>
2015-07-23 17:44:48 -0300

    Adicionar/remover custos, melhoria na exibição

--- src/main/resources/static/editar/assets/componentes/etapa/lista-de-custos.js
+++ src/main/resources/static/editar/assets/componentes/etapa/lista-de-custos.js
@@ -4,13 +4,48 @@ etapa.ListaDeCustos = {
 
   controller: function(args) {
     this.custos = args.campos;
+
+    this.adicionar = function() {
+      this.custos().push(new models.Custo());
+    };
+
+    this.remover = function(i) {
+      this.custos().splice(i, 1);
+    };
   },
 
   view: function(ctrl) {
     return m('.custos', [
-      ctrl.custos().map(function(custo) {
-        return m('.custo', [custo.descricao(), custo.moeda(), custo.valor()])
-      })
+      ctrl.custos().map(function(custo, i) {
+        return m('.custo', [
+          m('input.inline.inline-lg[type=text]', {
+            value: custo.descricao(),
+            onchange: m.withAttr('value', custo.descricao)
+          }),
+          ' ',
+          m('input.inline[type=text]', {
+            value: custo.moeda(),
+            onchange: m.withAttr('value', custo.moeda)
+          }),
+          ' ',
+          m('input.inline[type=text]', {
+            value: custo.valor(),
+            onchange: m.withAttr('value', custo.valor)
+          }),
+          ' ',
+          m('button.inline.remove-peq', {
+            onclick: ctrl.remover.bind(ctrl, i)
+          }, [
+            m("span.fa.fa-times")
+          ])
+        ]);
+      }),
+      m('button.adicionar-custo', {
+        onclick: ctrl.adicionar.bind(ctrl)
+      }, [
+        m("i.fa.fa-plus"),
+        " Adicionar custo "
+      ])
     ]);
   }
 };
