ce28b69b3ea5fcd2746a5e9fbf4f06c420eca3c5
Carlos Villela <cvillela@thoughtworks.com>
2015-07-27 13:55:36 -0300

    Beautify

--- src/test/javascript/lib/mithril-fake-xhr.js
+++ src/test/javascript/lib/mithril-fake-xhr.js
@@ -1,50 +1,51 @@
 'use strict';
 
-module.exports = function(base) {
+module.exports = function (base) {
 
   base = base || window;
-  var SaveXML = base.XMLHttpRequest; 
+  var SaveXML = base.XMLHttpRequest;
   var pending = {};
   var unexpectedRequests = 0;
-    
-  base.XMLHttpRequest = (function() {
+
+  base.XMLHttpRequest = (function () {
 
     function select(ctx, cb) {
-      Object.keys(pending).forEach(function(key){
-        var match = new RegExp('^'+key+'$').exec(ctx.method.toLowerCase() + ctx.url);
+      Object.keys(pending).forEach(function (key) {
+        var match = new RegExp('^' + key + '$').exec(ctx.method.toLowerCase() + ctx.url);
         if (match && match.length) {
           ctx.pending = pending[key];
-          cb(pending[key],ctx);
+          cb(pending[key], ctx);
         }
       });
     }
 
-    return function() {
+    return function () {
       this.$headers = {};
-      this.setRequestHeader = function(key, value) {
+      this.setRequestHeader = function (key, value) {
         this.$headers[key] = value;
       };
-      this.open = function(method, url, async, user, password) {
+      this.open = function (method, url, async, user, password) {
         this.method = method;
         this.url = url;
-        select(this, function(pending){
-          if (pending.passthrough){
+        select(this, function (pending) {
+          if (pending.passthrough) {
             pending.xhr = new SaveXML();
             return pending.xhr.open(method, url, async, user, password);
           }
         });
       };
-      this.send = function(data) {
-        var xhr = this, request = this.pending;
+      this.send = function (data) {
+        var xhr = this,
+          request = this.pending;
         xhr.readyState = 4;
         xhr.status = 400;
-        xhr.responseText='';
+        xhr.responseText = '';
         if (request && (request.data === undefined || JSON.stringify(request.data) === data)) {
-          if (request.passthrough){
+          if (request.passthrough) {
             var passthroughXhr = request.xhr;
-            passthroughXhr.onreadystatechange = function() {
+            passthroughXhr.onreadystatechange = function () {
               if (passthroughXhr.readyState === 4) {
-                var response = request.passthroughCb(passthroughXhr.status,JSON.parse(passthroughXhr.responseText));
+                var response = request.passthroughCb(passthroughXhr.status, JSON.parse(passthroughXhr.responseText));
                 request.response(response.data);
                 xhr.status = response.status;
                 xhr.responseText = JSON.stringify(response.data);
@@ -54,14 +55,12 @@ module.exports = function(base) {
             };
             passthroughXhr.$headers = xhr.$headers;
             passthroughXhr.send(data);
-          }
-          else {
+          } else {
             xhr.status = request.status();
             xhr.responseText = JSON.stringify(request.response());
             xhr.onreadystatechange();
           }
-        }
-        else {
+        } else {
           fake.unexpectedRequests += 1;
           xhr.onreadystatechange();
         }
@@ -70,23 +69,28 @@ module.exports = function(base) {
 
   }());
 
-  var fake = function(method, url, data, headers) {
+  var fake = function (method, url, data, headers) {
 
-    var key = method.toLowerCase()+url;
+    var key = method.toLowerCase() + url;
     var prop = m.prop('');
     var count = 0;
     var status = 200;
-    var response = function() {
-      count ++;
-      return prop.apply(this,arguments);
+    var response = function () {
+      count++;
+      return prop.apply(this, arguments);
     };
 
     var api = {
-      respondWith: function() {
+      respondWith: function () {
         switch (arguments.length) {
-          case 0: prop(''); break;
-          case 1: prop(arguments[0]); break;
-          default: {
+        case 0:
+          prop('');
+          break;
+        case 1:
+          prop(arguments[0]);
+          break;
+        default:
+          {
             status = arguments[0];
             prop(arguments[1]);
             break;
@@ -94,46 +98,51 @@ module.exports = function(base) {
         }
         return api;
       },
-      response:response,
-      passthrough: function(cb) {
+      response: response,
+      passthrough: function (cb) {
         pending[key].passthrough = true;
-        pending[key].passthroughCb = cb || function(s,d){return {status:s,data:d};};
+        pending[key].passthroughCb = cb || function (s, d) {
+          return {
+            status: s,
+            data: d
+          };
+        };
         return api;
       },
       get count() {
         return count;
       }
     };
-    
+
     pending[key] = {
-      status: function(){
+      status: function () {
         return status;
       },
-      response: function(){
-        return api.response.apply(this,arguments);
+      response: function () {
+        return api.response.apply(this, arguments);
       },
-      data:data
+      data: data
     };
     return api;
   };
 
   Object.defineProperty(fake, 'unexpectedRequests', {
-      get: function() {
-        // read once semantics
-        var count = unexpectedRequests;
-        unexpectedRequests = 0;
-        return count;
-      },
-      set: function(val) {
-          unexpectedRequests++;
-      }
+    get: function () {
+      // read once semantics
+      var count = unexpectedRequests;
+      unexpectedRequests = 0;
+      return count;
+    },
+    set: function (val) {
+      unexpectedRequests++;
+    }
   });
 
-  fake.reset = function() {
+  fake.reset = function () {
     pending = {};
     unexpectedRequests = 0;
   };
-  
+
   return fake;
 
 };
