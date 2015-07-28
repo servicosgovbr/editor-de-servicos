7a6105be6d5b1054a3222d1a31ee275c1c0c636e
Jean Kirchner <jean.kirchner@gmail.com>
2015-07-28 16:16:49 -0300

    Remove código css não usado, e diminui campos de texto para solicitantes - #177

--- src/main/javascript/componentes/solicitantes.js
+++ src/main/javascript/componentes/solicitantes.js
@@ -26,22 +26,19 @@ module.exports = {
         }, [
           m('h3', 'Tipo de solicitante'),
           m.component(require('componentes/editor-markdown'), {
-            rows: 5,
+            rows: 2,
             value: s.tipo(),
             onchange: m.withAttr('value', s.tipo)
           }),
-
           m('h3', 'Requisitos que tornam o solicitante elegível'),
           m.component(require('componentes/editor-markdown'), {
-            rows: 5,
+            rows: 2,
             value: s.requisitos(),
             onchange: m.withAttr('value', s.requisitos)
           }),
-
           m('button.inline.remover', {
             onclick: ctrl.remover.bind(ctrl, i)
-          }, [m('span.fa.fa-trash')]),
-
+          }, [m('span.fa.fa-trash')])
         ]);
       }),
 
