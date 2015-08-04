4d8a49030ade4728cc02412af5221cfc8733a4e2
Carlos Villela <cvillela@thoughtworks.com>
2015-08-04 01:50:30 -0300

    Usa API do Select2 v4.0.0

--- src/main/javascript/componentes/select2.js
+++ src/main/javascript/componentes/select2.js
@@ -63,13 +63,13 @@ module.exports = {
           }, ctrl.options));
 
           select2.on('change', function (e) {
-            if (!el.select2('val') || ctrl.prop() === el.select2('val')) {
+            if (!el.val() || ctrl.prop() === el.val()) {
               e.stopPropagation();
               return false;
             }
 
             m.startComputation();
-            ctrl.prop(el.select2('val'));
+            ctrl.prop(el.val());
             m.endComputation();
           });
         }
