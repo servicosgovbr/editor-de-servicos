c6f418990d1b94fe2ef9bb369de07ba0dd17bc5d
Carlos Villela <cvillela@thoughtworks.com>
2015-08-04 01:43:29 -0300

    Alinha colunas da tabela

--- src/main/javascript/bem-vindo.js
+++ src/main/javascript/bem-vindo.js
@@ -31,24 +31,28 @@ module.exports = {
       m('#bem-vindo', [
         m('h2', 'Bem-vindo!'),
 
-        m('input[type=search][placeholder="Buscar"].inline-lg', {
+        m('input[type=search][placeholder="Buscar"]', {
           oninput: m.withAttr('value', ctrl.filtro)
         }),
+
         m('table', [
           m('tr', [
-            m('th', 'Nome'),
-            m('th', 'Autor'),
-            m('th', 'Horário'),
-            m('th', 'Ações')
+            m('th[width="40%"]', 'Nome'),
+            m('th.center', 'Autor'),
+            m('th.center', 'Última atualização'),
+            m('th.right', 'Ações')
           ])
         ].concat(ctrl.servicosFiltrados().map(function (s) {
           return m('tr', [
-            m('td', s.id),
-            m('td', s.autor),
-            m('td', moment(s.horario).fromNow()),
-            m('td', m('a', {
+            m('td', s.id.replace(/\.xml$/, '').replace(/-/g, ' ')),
+            m('td.center', s.autor),
+            m('td.center', moment(s.horario).fromNow()),
+            m('td.right', m('a', {
               href: '/editar/servico/' + slugify(s.id)
-            }, 'editar'))
+            }, [
+              m('span.fa.fa-pencil'),
+              m.trust('&nbsp; Editar')
+            ]))
           ]);
         })))
       ])
