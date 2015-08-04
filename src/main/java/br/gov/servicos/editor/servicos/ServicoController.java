dc3cfe35230bd2814706f8b132c06de94948abfe
kchnr <jean.kirchner@gmail.com>
2015-08-04 01:15:07 -0300

    Cache para listagem de serviços - #197

--- src/main/java/br/gov/servicos/editor/servicos/ServicoController.java
+++ src/main/java/br/gov/servicos/editor/servicos/ServicoController.java
@@ -3,6 +3,7 @@ package br.gov.servicos.editor.servicos;
 import com.github.slugify.Slugify;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,7 @@ class ServicoController {
         this.slugify = slugify;
     }
 
+    @Cacheable("lista-servicos")
     @ResponseBody
     @RequestMapping(value = "/editar/api/servicos", method = GET)
     Iterable<Metadados> listar() throws IOException {
