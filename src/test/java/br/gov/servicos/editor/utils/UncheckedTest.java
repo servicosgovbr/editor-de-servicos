d2f9c4dcb6f1f972b3e5b7cfb54ca52aa12dd163
Carlos Villela <cvillela@thoughtworks.com>
2015-08-13 16:24:40 -0300

    Simplifica teste

--- src/test/java/br/gov/servicos/editor/utils/UncheckedTest.java
+++ src/test/java/br/gov/servicos/editor/utils/UncheckedTest.java
@@ -2,31 +2,20 @@ package br.gov.servicos.editor.utils;
 
 import org.junit.Test;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 public class UncheckedTest {
 
     @Test(expected = Exception.class)
     public void retornaVersaoSemExcecoesDaFuncao() throws Exception {
-        function(Unchecked.Function.unchecked(x -> {
+        Unchecked.Function.<Integer, Integer>unchecked(x -> {
             throw new Exception("");
-        }));
+        }).apply(1);
     }
 
     @Test(expected = Exception.class)
     public void retornaVersaoSemExcecoesDeSupplier() throws Exception {
-        supplier(Unchecked.Supplier.unchecked(() -> {
+        Unchecked.Supplier.<Integer>unchecked(() -> {
             throw new Exception("");
-        }));
-    }
-
-    private Integer function(Function<Integer, Integer> fn) {
-        return fn.apply(1);
-    }
-
-    private Integer supplier(Supplier<Integer> fn) {
-        return fn.get();
+        }).get();
     }
 
 }
