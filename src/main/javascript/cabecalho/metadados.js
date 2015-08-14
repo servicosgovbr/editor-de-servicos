f04189359c82e659bf4b93a300b93028e04e7223
Carlos Villela <cvillela@thoughtworks.com>
2015-08-14 18:05:56 -0300

    Exibe conectividade na barra - #267 #264

--- src/main/javascript/cabecalho/metadados.js
+++ src/main/javascript/cabecalho/metadados.js
@@ -1,5 +1,29 @@
 'use strict';
 
+var ping = function (element, isInitialized) {
+  if (!isInitialized) {
+    var e = jQuery(element);
+
+    var ok = function (resp) {
+      e.attr('class', 'fa fa-circle ok').attr('title', 'Conexão estabelecida');
+    };
+
+    var nok = function (resp) {
+      e.attr('class', 'fa fa-circle nok').attr('title', 'Conexão perdida');
+    };
+
+    var atualizarStatus = function() {
+      e.attr('class', 'fa fa-spinner fa-spin').attr('title', 'Verificando conexão');
+      m.request({
+        url: '/editar/api/ping',
+        background: true
+      }).then(ok, nok);
+    };
+
+    window.setInterval(atualizarStatus, 5000);
+  }
+};
+
 module.exports = {
 
   controller: function (args) {
@@ -13,6 +37,10 @@ module.exports = {
   view: function (ctrl) {
     if (m.route().indexOf('/editar/servico/') >= 0) {
       return m('#metadados', [
+
+      m('span#status-conexao', {
+          config: ping
+        }),
 
       m('button#salvar', {
           onclick: _.bind(ctrl.salvar, ctrl)
