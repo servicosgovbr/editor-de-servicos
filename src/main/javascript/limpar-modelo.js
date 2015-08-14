26b790c37bde817b9c992e92bed3c0cb23461759
kchnr <jean.kirchner@gmail.com>
2015-08-13 21:32:27 -0300

    limpeza de campos do modelo, removendo campos principais se todos os campos filhos estiverem vazios - #239

---
+++ src/main/javascript/limpar-modelo.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var l = function () {
+  window.console.log.apply(window.console, arguments);
+};
+l(null);
+
+var trimProp = function (prop) {
+  prop(_.trim(prop()));
+};
+
+var limparSolicitante = function (solicitante) {
+  [solicitante.tipo, solicitante.requisitos].forEach(trimProp);
+  return (solicitante.tipo() || solicitante.requisitos()) ? solicitante : null;
+};
+
+var limparCaso = function (caso, fnLimpar) {
+  caso.descricao(_.trim(caso.descricao()));
+  caso.campos(_.compact(caso.campos().map(fnLimpar)));
+  return (caso.descricao() || !_.isEmpty(caso.campos())) ? caso : null;
+};
+
+var limparCasos = function (obj, fnLimpar) {
+  obj.casoPadrao(limparCaso(obj.casoPadrao(), fnLimpar));
+  obj.outrosCasos(_.compact(obj.outrosCasos().map(function (caso) {
+    return limparCaso(caso, fnLimpar);
+  })));
+  return obj.casoPadrao() || !_.isEmpty(obj.outrosCasos()) ? obj : null;
+};
+
+var limparDocumentos = function (documentos) {
+  return limparCasos(documentos, _.trim);
+};
+
+var limparCusto = function (custo) {
+  custo.descricao(_.trim(custo.descricao()));
+  custo.moeda(_.trim(custo.moeda()));
+  custo.valor(_.trim(custo.valor()));
+
+  return (custo.descricao() || custo.moeda() || custo.valor()) ? custo : null;
+};
+
+var limparCustos = function (custos) {
+  return limparCasos(custos, limparCusto);
+};
+
+var limparCanalDePrestacao = function (canal) {
+  canal.tipo(_.trim(canal.tipo()));
+  canal.descricao(_.trim(canal.descricao()));
+
+  return (canal.tipo() || canal.descricao()) ? canal : null;
+};
+
+var limparCanaisDePrestacao = function (canaisDePrestacao) {
+  return limparCasos(canaisDePrestacao, limparCanalDePrestacao);
+};
+
+var limparEtapa = function (etapa) {
+  [etapa.titulo, etapa.descricao].forEach(trimProp);
+
+  etapa.documentos(limparDocumentos(etapa.documentos()));
+  etapa.custos(limparCustos(etapa.custos()));
+  etapa.canaisDePrestacao(limparCanaisDePrestacao(etapa.canaisDePrestacao()));
+
+  return (etapa.titulo() || etapa.descricao() || !_.isEmpty(etapa.documentos()) || !_.isEmpty(etapa.custos()) || !_.isEmpty(etapa.canaisDePrestacao())) ? etapa : null;
+};
+
+module.exports = function (servico) {
+  [servico.nome, servico.sigla, servico.descricao].forEach(trimProp);
+
+  servico.nomesPopulares(_.compact(servico.nomesPopulares().map(_.trim)));
+  servico.solicitantes(_.compact(servico.solicitantes().map(limparSolicitante)));
+  servico.etapas(_.compact(servico.etapas().map(limparEtapa)));
+
+  return servico;
+
+};
---
+++ src/test/javascript/limpar-modelo-test.js
@@ -0,0 +1,263 @@
+'use strict';
+
+var modelos = require('modelos');
+var limparModelo = require('limpar-modelo');
+var Solicitante = modelos.Solicitante;
+var Etapa = modelos.Etapa;
+var Documentos = modelos.Documentos;
+var Custos = modelos.Custos;
+var Custo = modelos.Custo;
+var CanaisDePrestacao = modelos.CanaisDePrestacao;
+var CanalDePrestacao = modelos.CanalDePrestacao;
+var Caso = modelos.Caso;
+
+describe('deve limpar >', function () {
+
+  var servico;
+
+  beforeEach(function () {
+    m.route.param = function () {};
+    servico = new modelos.Servico({
+      nome: '        ',
+      sigla: '   ABC  ',
+      descricao: '  blah           ',
+      nomesPopulares: [null, '  nome  ', undefined, '']
+    });
+  });
+
+  it('nome do servico', function () {
+    expect(limparModelo(servico).nome()).toBe('');
+  });
+
+  it('sigla do servico', function () {
+    expect(limparModelo(servico).sigla()).toBe('ABC');
+  });
+
+  it('descricao do servico', function () {
+    expect(limparModelo(servico).descricao()).toBe('blah');
+  });
+
+  it('nomes populares do servico', function () {
+    expect(limparModelo(servico).nomesPopulares()).toEqual(['nome']);
+  });
+
+  describe('solicitantes >', function () {
+    beforeEach(function () {
+      servico = new modelos.Servico({
+        solicitantes: [
+         new Solicitante(),
+         new Solicitante({
+            tipo: '  ',
+            requisitos: '      '
+          }),
+         new Solicitante({
+            tipo: ' a ',
+            requisitos: '       '
+          })
+       ]
+      });
+    });
+
+    it('filtrar vazios', function () {
+      expect(limparModelo(servico).solicitantes().length).toBe(1);
+    });
+
+    it('campos tipo e requisios', function () {
+      servico = limparModelo(servico);
+
+      expect(servico.solicitantes()[0].tipo()).toBe('a');
+      expect(servico.solicitantes()[0].requisitos()).toBe('');
+    });
+  });
+
+  describe('etapas >', function () {
+    beforeEach(function () {
+      servico = new modelos.Servico({
+        etapas: [
+          new Etapa(),
+          new Etapa({
+            documentos: new Documentos(),
+            custos: new Custos(),
+            canaisDePrestacao: new CanaisDePrestacao()
+          }),
+          new Etapa({
+            titulo: ' titulo '
+          })
+        ]
+      });
+    });
+
+    it('filtrar vazias', function () {
+      expect(limparModelo(servico).etapas().length).toBe(1);
+    });
+
+    it('titulo e descricao', function () {
+      var etapa = limparModelo(servico).etapas()[0];
+
+      expect(etapa.titulo()).toBe('titulo');
+      expect(etapa.descricao()).toBe('');
+    });
+
+    describe('documentos >', function () {
+      beforeEach(function () {
+        servico = new modelos.Servico({
+          etapas: [
+            new Etapa({
+              documentos: new Documentos({
+                casoPadrao: new Caso('', {
+                  padrao: true,
+                  campos: ['', null, undefined, ' doc ']
+                }),
+                outrosCasos: [
+                  new Caso(), new Caso('', {
+                    descricao: '   '
+                  }),
+                  new Caso('', {
+                    descricao: '   a   '
+                  }),
+                  new Caso('', {
+                    campos: ['', null, undefined, ' le doc        ']
+                  })]
+              })
+            })]
+        });
+      });
+
+      it('caso padrão', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+        expect(etapa.documentos()).toBeDefined();
+        expect(etapa.documentos().casoPadrao()).toBeDefined();
+        expect(etapa.documentos().casoPadrao().campos()).toEqual(['doc']);
+      });
+
+      it('outros casos', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+        expect(etapa.documentos().outrosCasos().length).toBe(2);
+
+        var caso1 = etapa.documentos().outrosCasos()[0];
+        var caso2 = etapa.documentos().outrosCasos()[1];
+
+        expect(caso1.descricao()).toBe('a');
+        expect(caso2.campos()).toEqual(['le doc']);
+      });
+    });
+
+    describe('custos >', function () {
+      beforeEach(function () {
+        servico = new modelos.Servico({
+          etapas: [
+            new Etapa({
+              custos: new Custos({
+                casoPadrao: new Caso('', {
+                  campos: [new Custo(), new Custo({
+                    descricao: ' blah ',
+                    moeda: ' R$',
+                    valor: '   10   '
+                  })]
+                }),
+                outrosCasos: [new Caso(), new Caso('', {
+                  descricao: 'a'
+                }), new Caso('', {
+                  campos: [new Custo(), new Custo({
+                    descricao: ' blah ',
+                    moeda: ' R$',
+                    valor: '   10   '
+                  }), new Custo()]
+                }), new Caso()]
+              })
+            })]
+        });
+      });
+
+      it('caso padrão', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+
+        expect(etapa.custos()).toBeDefined();
+        expect(etapa.custos().casoPadrao()).toBeDefined();
+        expect(etapa.custos().casoPadrao().campos().length).toBe(1);
+
+        expect(etapa.custos().casoPadrao().campos()[0].descricao()).toBe('blah');
+        expect(etapa.custos().casoPadrao().campos()[0].moeda()).toBe('R$');
+        expect(etapa.custos().casoPadrao().campos()[0].valor()).toBe('10');
+      });
+
+      it('outros casos', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+
+        expect(etapa.custos().outrosCasos()).toBeDefined();
+        expect(etapa.custos().outrosCasos().length).toBe(2);
+
+        var caso1 = etapa.custos().outrosCasos()[0];
+        var caso2 = etapa.custos().outrosCasos()[1];
+
+        expect(caso1.descricao()).toBe('a');
+
+        expect(caso2.campos().length).toBe(1);
+        expect(caso2.campos()[0].descricao()).toBe('blah');
+        expect(caso2.campos()[0].moeda()).toBe('R$');
+        expect(caso2.campos()[0].valor()).toBe('10');
+      });
+
+    });
+
+    describe('canais de prestação >', function () {
+      beforeEach(function () {
+        servico = new modelos.Servico({
+          etapas: [new Etapa({
+            canaisDePrestacao: new CanaisDePrestacao({
+              casoPadrao: new Caso('', {
+                descricao: ' a ',
+                campos: [new CanalDePrestacao(), new CanalDePrestacao({
+                  tipo: '   canal caso padrao  ',
+                  descricao: ' desc  '
+                }), new CanalDePrestacao({
+                  tipo: '   ',
+                  descricao: '     '
+                })]
+              }),
+              outrosCasos: [new Caso(), new Caso(), new Caso('', {
+                  descricao: ' outros casos '
+                }), new Caso('', {
+                  campos: [new CanalDePrestacao(), new CanalDePrestacao({
+                    tipo: 'tipo outros casos  ',
+                    descricao: '  descricao canal outros casos'
+                  }), new CanalDePrestacao()]
+                })
+              ]
+            })
+          })]
+        });
+      });
+
+      it('caso padrão', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+
+        expect(etapa.canaisDePrestacao()).toBeDefined();
+        expect(etapa.canaisDePrestacao().casoPadrao()).toBeDefined();
+        expect(etapa.canaisDePrestacao().casoPadrao().descricao()).toBe('a');
+        expect(etapa.canaisDePrestacao().casoPadrao().campos().length).toBe(1);
+
+        var canal = etapa.canaisDePrestacao().casoPadrao().campos()[0];
+        expect(canal.tipo()).toBe('canal caso padrao');
+        expect(canal.descricao()).toBe('desc');
+      });
+
+      it('outros casos', function () {
+        var etapa = limparModelo(servico).etapas()[0];
+
+        expect(etapa.canaisDePrestacao()).toBeDefined();
+        expect(etapa.canaisDePrestacao().outrosCasos()).toBeDefined();
+        expect(etapa.canaisDePrestacao().outrosCasos().length).toBe(2);
+
+        var caso1 = etapa.canaisDePrestacao().outrosCasos()[0];
+        var caso2 = etapa.canaisDePrestacao().outrosCasos()[1];
+
+        expect(caso1.descricao()).toBe('outros casos');
+
+        expect(caso2.campos().length).toBe(1);
+        expect(caso2.campos()[0].tipo()).toBe('tipo outros casos');
+        expect(caso2.campos()[0].descricao()).toBe('descricao canal outros casos');
+      });
+    });
+  });
+});
