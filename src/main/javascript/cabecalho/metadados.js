a96d02f84ff7526925be5135ccc22c47b77bed8c
Carlos Villela <cvillela@thoughtworks.com>
2015-08-15 00:53:38 -0300

    Extrai componente - #267 #264

--- src/main/javascript/cabecalho/metadados.js
+++ src/main/javascript/cabecalho/metadados.js
@@ -1,41 +1,5 @@
 'use strict';
 
-var ping = function (element, isInitialized) {
-  if (!isInitialized) {
-    var e = jQuery(element);
-
-    var ok = function (resp) {
-      if(resp.login) {
-        e.attr('class', 'fa fa-circle ok').attr('title', 'Conexão estabelecida').html('');
-
-      } else {
-        e.attr('class', 'fa fa-circle nok').attr('title', 'Sessão expirada')
-          .html('<span class="mensagem">Sua sessão expirou! <a href="/editar/" target="_blank">Clique aqui para entrar novamente.</a></span>');
-      }
-    };
-
-    var nok = function (resp) {
-      e.attr('class', 'fa fa-circle nok').attr('title', 'Conexão perdida').html('');
-    };
-
-    var atualizarStatus = function () {
-      e.attr('class', 'fa fa-spinner fa-spin').attr('title', 'Verificando conexão').html('');
-      m.request({
-        url: '/editar/api/ping',
-        extract: function(xhr) {
-          if(/\/editar\/login/.test(xhr.responseURL)) {
-            return '{"login": null}';
-          }
-          return xhr.responseText;
-        },
-        background: true
-      }).then(ok, nok);
-    };
-
-    window.setInterval(atualizarStatus, 5000);
-  }
-};
-
 module.exports = {
 
   controller: function (args) {
@@ -50,9 +14,7 @@ module.exports = {
     if (m.route().indexOf('/editar/servico/') >= 0) {
       return m('#metadados', [
 
-      m('span#status-conexao', {
-          config: ping
-        }),
+      m.component(require('componentes/status-conexao')),
 
       m('button#salvar', {
           onclick: _.bind(ctrl.salvar, ctrl)
---
+++ src/main/javascript/componentes/status-conexao.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var httpOk = function (resp) {
+  if (resp.login) {
+    this.attr('class', 'fa fa-circle ok')
+      .attr('title', 'Conexão estabelecida')
+      .html('');
+
+  } else {
+    this.attr('class', 'fa fa-circle nok')
+      .attr('title', 'Sessão expirada')
+      .html('<span class="mensagem">Sua sessão expirou! <a href="/editar/" target="_blank">Clique aqui para entrar novamente.</a></span>');
+  }
+};
+
+var httpError = function (resp) {
+  this.attr('class', 'fa fa-circle nok')
+    .attr('title', 'Conexão perdida')
+    .html('');
+};
+
+var atualizarStatus = function () {
+  this.attr('class', 'fa fa-spinner fa-spin')
+    .attr('title', 'Verificando conexão')
+    .html('');
+
+  m.request({
+    url: '/editar/api/ping',
+    extract: function (xhr) {
+      if (/\/editar\/login/.test(xhr.responseURL)) {
+        return '{"login": null}';
+      }
+      return xhr.responseText;
+    },
+    background: true
+  }).then(_.bind(httpOk, this), _.bind(httpError, this));
+};
+
+var config = function (element, isInitialized) {
+  if (isInitialized) {
+    return;
+  }
+
+  var e = jQuery(element);
+  var fn = _.bind(atualizarStatus, e);
+
+  window.setInterval(fn, 30000);
+  fn();
+};
+
+module.exports = {
+
+  view: function () {
+    return m('span#status-conexao', {
+      config: config
+    });
+  }
+};
