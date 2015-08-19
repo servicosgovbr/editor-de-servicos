4ddb1c6e9addfb434d509b55cd7bfffe44e2fc92
Samantha Rosa and Glauber Ramos <sah.rosa@gmail.com>
2015-08-19 17:38:44 -0300

    cria mensagem de alerta para sessão expirada #279

--- src/main/javascript/componentes/status-conexao.js
+++ src/main/javascript/componentes/status-conexao.js
@@ -7,9 +7,8 @@ var httpOk = function (resp) {
       .html('');
 
   } else {
-    this.attr('class', 'fa fa-circle nok')
-      .attr('title', 'Sessão expirada')
-      .html('<span class="mensagem">Sua sessão expirou! <a href="/editar/" target="_blank">Clique aqui para entrar novamente.</a></span>');
+    this.attr('class', 'mensagem erro')
+      .html('Sua sessão expirou. <a href="/editar/" target="_blank">Clique aqui para entrar novamente</a>');
   }
 };
 
