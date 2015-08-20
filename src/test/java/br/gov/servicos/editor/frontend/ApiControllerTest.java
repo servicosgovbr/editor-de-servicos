9d5f4ca90d3a86ce2e9c6632935b3e91ef77e3ee
Carlos Villela <cvillela@thoughtworks.com>
2015-08-19 21:09:25 -0300

    Corrige testes

--- src/test/java/br/gov/servicos/editor/frontend/ApiControllerTest.java
+++ src/test/java/br/gov/servicos/editor/frontend/ApiControllerTest.java
@@ -2,22 +2,33 @@ package br.gov.servicos.editor.frontend;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.userdetails.User;
 
 import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ApiControllerTest {
-
-    private ApiController controller;
 
     public static final User USUARIO = new User("Fulano de Tal", "", emptyList());
 
+    @Mock
+    VCGE vcge;
+
+    @Mock
+    Orgaos orgaos;
+
+    ApiController controller;
+
     @Before
     public void setUp() throws Exception {
-        controller = new ApiController();
+        controller = new ApiController(vcge, orgaos);
     }
 
     @Test
@@ -26,5 +37,17 @@ public class ApiControllerTest {
 
         assertThat(ping.getLogin(), is("Fulano de Tal"));
         assertThat(System.currentTimeMillis() - ping.getHorario(), is(lessThan(1000L)));
+    }
+
+    @Test
+    public void listaOrgaosDisponiveis() throws Exception {
+        controller.orgaos();
+        verify(orgaos).get();
+    }
+
+    @Test
+    public void listaCategoriasDoVcgeDisponiveis() throws Exception {
+        controller.vcge();
+        verify(vcge).get();
     }
 }
