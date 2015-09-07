9566468d787fcd016ae2577db00ab506091858dc
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-07 14:01:32 -0300

    Reverte remoção de Orgão - #209

---
+++ src/main/java/br/gov/servicos/editor/servicos/Orgao.java
@@ -0,0 +1,17 @@
+package br.gov.servicos.editor.servicos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Wither;
+
+@Data
+@Wither
+@NoArgsConstructor
+@AllArgsConstructor
+public class Orgao {
+
+    String id;
+    String nome;
+
+}
