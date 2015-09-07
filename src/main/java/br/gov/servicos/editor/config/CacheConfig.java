f763bab15ac6d0c052572e184b0048165cb03c83
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-07 11:57:20 -0300

    Adiciona Siorg ao cache - #209

--- src/main/java/br/gov/servicos/editor/config/CacheConfig.java
+++ src/main/java/br/gov/servicos/editor/config/CacheConfig.java
@@ -3,18 +3,34 @@ package br.gov.servicos.editor.config;
 import br.gov.servicos.editor.cartas.Carta;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
+    public static final String METADADOS = "metadados";
+    public static final String ORGAOS = "orgaos";
+    public static final String ORGAO_SIORG = "orgaoSiorg";
+    public static final String VCGE = "vcge";
+
+
     @Bean
     public CacheManager cacheManager() {
-        return new GuavaCacheManager();
+        SimpleCacheManager manager = new SimpleCacheManager();
+        manager.setCaches(asList(
+                new ConcurrentMapCache(METADADOS),
+                new ConcurrentMapCache(ORGAOS),
+                new ConcurrentMapCache(ORGAO_SIORG),
+                new ConcurrentMapCache(VCGE)
+        ));
+        return manager;
     }
 
     @Bean
