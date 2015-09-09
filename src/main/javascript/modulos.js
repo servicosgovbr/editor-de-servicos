fc1bf9649c0726ba1dd1dc1e619a74219f251c85
Carlos Villela <cvillela@thoughtworks.com>
2015-09-08 21:01:03 -0300

    Remove codigo temporário de migracao - #209

--- src/main/javascript/modulos.js
+++ src/main/javascript/modulos.js
@@ -5,7 +5,6 @@ module.exports = function (m) {
 
   m.route(document.body, '/editar', {
     '/editar': require('bem-vindo'),
-    '/editar/servico/migracao': require('migracao'),
     '/editar/erro': require('erro'),
     '/editar/servico/:id': require('editor-de-servicos')
   });
