72467bbc8ac9125202ff59e1002f308d885ac3e6
Carlos Villela <cvillela@thoughtworks.com>
2015-09-11 03:07:20 -0300

    Adiciona teste para o VCGE

---
+++ src/test/java/br/gov/servicos/editor/frontend/VCGETest.java
@@ -0,0 +1,28 @@
+package br.gov.servicos.editor.frontend;
+
+import br.gov.servicos.editor.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@IntegrationTest("flags.esquentar.cache=false")
+public class VCGETest {
+
+    @Autowired
+    VCGE vcge;
+
+    @Test
+    public void obtemEstruturaDoVCGE() throws Exception {
+        assertThat(vcge.get(), allOf(is(notNullValue()), not(isEmptyString())));
+    }
+}
