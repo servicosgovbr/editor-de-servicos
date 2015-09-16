06cb02ba776389fe84b47691340798479ecd6f0f
Carlos Villela <cvillela@thoughtworks.com>
2015-09-16 20:11:07 -0300

    Remove @Import morto, inline num bean que só era usado em um lugar - #77

--- src/main/java/br/gov/servicos/editor/oauth2/google/config/SecurityWebAppInitializer.java
+++ src/main/java/br/gov/servicos/editor/oauth2/google/config/SecurityWebAppInitializer.java
@@ -1,19 +1,28 @@
 package br.gov.servicos.editor.oauth2.google.config;
 
+import br.gov.servicos.editor.oauth2.google.security.GoogleTokenServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 import javax.annotation.Resource;
 
@@ -25,8 +34,7 @@ import static org.springframework.security.oauth2.common.AuthenticationScheme.qu
 @Configuration
 @EnableOAuth2Client
 @EnableWebSecurity
-@ImportResource({"classpath:security-context.xml"})
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+public class SecurityWebAppInitializer extends WebSecurityConfigurerAdapter {
 
     @Resource
     @Qualifier("accessTokenRequest")
@@ -50,15 +58,11 @@ public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationIni
     @Value("${google.auth.scope}")
     String authScope;
 
-    /**
-     * This authentication entry point is used for all the unauthenticated or unauthorised sessions to be directed to the
-     * /googleLogin URL which is then intercepted by the oAuth2AuthenticationProcessingFilter to trigger authentication from
-     * Google.
-     */
-    @Bean
-    AuthenticationEntryPoint clientAuthenticationEntryPoint() {
-        return new LoginUrlAuthenticationEntryPoint("/googleLogin");
-    }
+    @Autowired
+    OAuth2ClientContextFilter oauth2ClientContextFilter;
+
+    @Autowired
+    OAuth2ClientAuthenticationProcessingFilter oauth2ClientAuthenticationProcessingFilter;
 
     @Bean
     @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -87,4 +91,33 @@ public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationIni
         return details;
     }
 
+    @Bean
+    OAuth2ClientAuthenticationProcessingFilter oauth2ClientAuthenticationProcessingFilter(
+            OAuth2RestTemplate restTemplate,
+            GoogleTokenServices tokenServices
+    ) {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter("/googleLogin");
+        filter.setRestTemplate(restTemplate);
+        filter.setTokenServices(tokenServices);
+        return filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/googleLogin"));
+
+        http.addFilterAfter(oauth2ClientContextFilter, ExceptionTranslationFilter.class);
+        http.addFilterBefore(oauth2ClientAuthenticationProcessingFilter, FilterSecurityInterceptor.class);
+
+        http.logout().logoutUrl("/editar/logout");
+        http.anonymous().disable();
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/editar/info").permitAll()
+                .antMatchers("/editar/health").permitAll()
+                .antMatchers("/editar/assets/**").permitAll()
+                .antMatchers("/editar/fonts/**").permitAll()
+                .anyRequest().fullyAuthenticated();
+    }
 }
