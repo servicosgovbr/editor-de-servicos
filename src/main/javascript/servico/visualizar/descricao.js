5e9fb5fec3dcc5aae2a61f64755fd1e5213c61b4
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-05 15:40:56 -0300

    Permite visualizar markdown na descrição de um serviço - #75

--- src/main/javascript/servico/visualizar/descricao.js
+++ src/main/javascript/servico/visualizar/descricao.js
@@ -4,12 +4,13 @@ module.exports = {
 
   controller: function (args) {
     this.servico = args;
+    this.converter = new window.showdown.Converter();
   },
 
   view: function (ctrl) {
     return m('#servico-descricao', [
         m('h3.subtitulo-servico', 'O que é?'),
-        m('.markdown', ctrl.servico.descricao())
+        m('.markdown', m.trust(ctrl.converter.makeHtml(ctrl.servico.descricao())))
     ]);
   }
 };
