7c3f7ca29d130ea9ab86a0dd40547b31ed9eade2
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-05 16:30:28 -0300

    Formatação

--- src/main/javascript/servico/visualizar/solicitantes.js
+++ src/main/javascript/servico/visualizar/solicitantes.js
@@ -3,25 +3,25 @@
 module.exports = {
 
     controller: function (args) {
-        this.servico = args;
-        this.converter = new window.showdown.Converter();
+      this.servico = args;
+      this.converter = new window.showdown.Converter();
     },
 
     view: function (ctrl) {
-        if (_.isEmpty(ctrl.servico.solicitantes())) {
-            return m('');
-        } else {
-            return m('#servico-solicitantes', [
+      if (_.isEmpty(ctrl.servico.solicitantes())) {
+        return m('');
+      } else {
+        return m('#servico-solicitantes', [
                 m('h3.subtitulo-servico', 'Quem pode utilizar este serviço?'),
-                ctrl.servico.solicitantes().map(function(s) {
-                    return m('.solicitantes markdown margem-solicitantes', [
-                        m('h4', m.trust(s.tipo())),
-                        m.trust(ctrl.converter.makeHtml(s.requisitos()))
-                    ]);
+                ctrl.servico.solicitantes().map(function (s) {
+                return m('.solicitantes markdown margem-solicitantes', [
+                        m('h4', m.trust(s.tipo()),
+                      m.trust(ctrl.converter.makeHtml(s.requisitos()))
+                      ]);
                 })
 
 
-            ]);
+              ]);
         }
-    }
-};
+      }
+    };
