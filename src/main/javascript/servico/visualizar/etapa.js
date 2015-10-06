d1138de6f1d80d1b68a94c1bf2efa49d8791a9f7
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-06 17:52:31 -0300

    Adiciona informações padrões da etapa - #75

--- src/main/javascript/servico/visualizar/etapa.js
+++ src/main/javascript/servico/visualizar/etapa.js
@@ -8,11 +8,25 @@ module.exports = {
   },
 
   view: function (ctrl) {
+    var documentos = function (etapa) {
+      if (!_.isEmpty(etapa.documentos())) {
+        window.console.log(etapa.documentos());
+        return m('.subtitulo-etapa', [
+                m('p.titulo-documento', 'Documentação'),
+                m('p.info-etapa', 'Documentação necessária'),
+                m('ul', etapa.documentos().casoPadrao().campos().map(function (campo) {
+            return m('li', m('span', campo.descricao()));
+          }))]);
+      }
+      return m.component(require('servico/visualizar/view-vazia'));
+    };
+
     return m('', ctrl.etapas.map(function (etapa, index) {
       return m('.etapas', [
                 m('p.circle', index + 1),
                 m('h4.etapa', etapa.titulo() ? etapa.titulo() : 'Acesse o serviço'),
-                m('.etapa markdown', m.trust(ctrl.converter.makeHtml(etapa.descricao())))
+                m('.etapa markdown', m.trust(ctrl.converter.makeHtml(etapa.descricao()))),
+                documentos(etapa)
             ]);
     }));
 
