25ef187ca03fc4a1606a0c8c4a46c99fe26c6635
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-07 17:14:55 -0300

    Modulariza visualização de canais de prestação em etapa - #75

---
+++ src/main/javascript/servico/visualizar/canais-de-prestacao.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var referencia = require('referencia');
+
+module.exports = {
+
+  controller: function (args) {
+    this.canaisDePrestacao = args;
+    this.tipos = referencia.tiposDeCanalDePrestacaoVisualizar;
+    this.converter = new window.showdown.Converter();
+  },
+
+  view: function (ctrl) {
+
+    var descricaoView = function (campo) {
+      if (ctrl.tipos[campo.tipo()].destacado) {
+        return m('a', {
+          href: campo.descricao()
+        }, ctrl.tipos[campo.tipo()].descricaoLink);
+      }
+      return m('', m.trust(ctrl.converter.makeHtml(campo.descricao())));
+    };
+
+    var camposView = function (campos) {
+      return campos.map(function (campo) {
+        return m('li', [
+                    m('span', ctrl.tipos[campo.tipo()].text + ': '),
+                    descricaoView(campo)
+                ]);
+      });
+    };
+
+    var canalPadraoView = function (cp) {
+      return m('ul', camposView(cp.campos()));
+    };
+
+    var outrosCanaisView = function (casos) {
+      return casos.map(function (caso) {
+        return m('ul', [
+                    m('.info-etapa', caso.descricao()),
+                    camposView(caso.campos())
+                ]);
+      });
+    };
+
+    if (!_.isEmpty(ctrl.canaisDePrestacao)) {
+      return m('.subtitulo-etapa', [
+                    m('p.titulo-documento', 'Canais de prestação'),
+                    m('p.info-etapa', 'Canais de prestação padrão'),
+                    canalPadraoView(ctrl.canaisDePrestacao.casoPadrao()),
+                    outrosCanaisView(ctrl.canaisDePrestacao.outrosCasos())
+                ]);
+    }
+    return m.component(require('servico/visualizar/view-vazia'));
+
+  }
+};
--- src/main/javascript/servico/visualizar/etapa.js
+++ src/main/javascript/servico/visualizar/etapa.js
@@ -1,59 +1,13 @@
 'use strict';
 
-var referencia = require('referencia');
-
 module.exports = {
 
   controller: function (args) {
     this.etapas = args;
     this.converter = new window.showdown.Converter();
-    this.tipos = referencia.tiposDeCanalDePrestacaoVisualizar;
   },
 
   view: function (ctrl) {
-
-    var canalPadrao = function (cp) {
-      return m('ul', cp.campos().map(function (campo) {
-        return m('li', [
-                  m('span', ctrl.tipos[campo.tipo()].text + ': '),
-                  ctrl.tipos[campo.tipo()].destacado ?
-                      m('a', {
-            href: campo.descricao()
-          }, ctrl.tipos[campo.tipo()].descricaoLink) :
-                      m('', m.trust(ctrl.converter.makeHtml(campo.descricao())))
-              ]);
-      }));
-    };
-
-    var outrosCanais = function (casos) {
-      return casos.map(function (caso) {
-        return m('ul', [
-                m('.info-etapa', caso.descricao()),
-                caso.campos().map(function (campo) {
-            return m('li', [
-                        m('span', ctrl.tipos[campo.tipo()].text + ': '),
-                        ctrl.tipos[campo.tipo()].destacado ?
-                            m('a', {
-                href: campo.descricao()
-              }, ctrl.tipos[campo.tipo()].descricaoLink) :
-                            m('', m.trust(ctrl.converter.makeHtml(campo.descricao())))
-                    ]);
-          })
-            ]);
-      });
-    };
-
-    var canaisDePrestacao = function (etapa) {
-      if (!_.isEmpty(etapa.canaisDePrestacao())) {
-        return m('.subtitulo-etapa', [
-                m('p.titulo-documento', 'Canais de prestação'),
-                m('p.info-etapa', 'Canais de prestação padrão'),
-                canalPadrao(etapa.canaisDePrestacao().casoPadrao()),
-                outrosCanais(etapa.canaisDePrestacao().outrosCasos())
-            ]);
-      }
-      return m.component(require('servico/visualizar/view-vazia'));
-    };
 
     return m('', ctrl.etapas.map(function (etapa, index) {
       return m('.etapas', [
@@ -62,7 +16,7 @@ module.exports = {
                 m('.etapa markdown', m.trust(ctrl.converter.makeHtml(etapa.descricao()))),
                 m.component(m.component(require('servico/visualizar/documentos'), etapa.documentos())),
                 m.component(m.component(require('servico/visualizar/custos'), etapa.custos())),
-                canaisDePrestacao(etapa),
+                m.component(m.component(require('servico/visualizar/canais-de-prestacao'), etapa.canaisDePrestacao()))
             ]);
     }));
 
