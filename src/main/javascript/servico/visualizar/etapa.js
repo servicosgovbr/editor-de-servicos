30dfd2893a939910b38f94a185dc262a5f87ff9f
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-07 11:30:04 -0300

    Extrai visualização de casos da view - #75

--- src/main/javascript/servico/visualizar/etapa.js
+++ src/main/javascript/servico/visualizar/etapa.js
@@ -8,23 +8,30 @@ module.exports = {
   },
 
   view: function (ctrl) {
+    var casoPadrao = function (cp) {
+      return m('ul', cp.campos().map(function (campo) {
+        return m('li', m('span', campo.descricao()));
+      }));
+    };
+
+    var outrosCasos = function (casos) {
+      return casos.map(function (caso) {
+        return m('ul', [
+                m('.info-etapa', caso.descricao()),
+                caso.campos().map(function (c) {
+            return m('li', m('span', c.descricao()));
+          })
+            ]);
+      });
+    };
+
     var documentos = function (etapa) {
       if (!_.isEmpty(etapa.documentos())) {
         return m('.subtitulo-etapa', [
                 m('p.titulo-documento', 'Documentação'),
                 m('p.info-etapa', 'Documentação necessária'),
-                m('ul', etapa.documentos().casoPadrao().campos().map(function (campo) {
-            return m('li', m('span', campo.descricao()));
-          })),
-                etapa.documentos().outrosCasos().map(function (caso) {
-            return m('ul', [
-                                m('.info-etapa', caso.descricao()),
-                                caso.campos().map(function (c) {
-                return m('li',
-                  m('span', c.descricao()));
-              })
-                                ]);
-          })
+                casoPadrao(etapa.documentos().casoPadrao()),
+                outrosCasos(etapa.documentos().outrosCasos())
         ]);
       }
       return m.component(require('servico/visualizar/view-vazia'));
@@ -33,7 +40,7 @@ module.exports = {
     return m('', ctrl.etapas.map(function (etapa, index) {
       return m('.etapas', [
                 m('p.circle', index + 1),
-                m('h4.etapa', etapa.titulo() ? etapa.titulo() : 'Acesse o serviço'),
+                m('h4.etapa', etapa.titulo() ? etapa.titulo() : 'acesse o serviço'),
                 m('.etapa markdown', m.trust(ctrl.converter.makeHtml(etapa.descricao()))),
                 documentos(etapa)
             ]);
