ee120809d4e91340a6b3d5ae2561e261fc0472d1
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-07 16:09:19 -0300

    Modulariza visualização de documentos em etapa - #75

---
+++ src/main/javascript/servico/visualizar/documentos.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+
+  controller: function (args) {
+    this.documentos = args;
+  },
+
+  view: function (ctrl) {
+
+    var camposView = function (campos) {
+      return campos.map(function (campo) {
+        return m('li', m('span', campo.descricao()));
+      });
+    };
+
+    var casoPadraoView = function (cp) {
+      return m('ul', camposView(cp.campos()));
+    };
+
+    var outrosCasosView = function (casos) {
+      return casos.map(function (caso) {
+        return m('ul.caso-descricao', [
+                    m('.info-etapa', caso.descricao()),
+                    camposView(caso.campos())
+                ]);
+      });
+    };
+
+    if (!_.isEmpty(ctrl.documentos)) {
+      return m('.subtitulo-etapa', [
+                m('p.titulo-documento', 'Documentação'),
+                m('p.info-etapa', 'Documentação necessária'),
+                casoPadraoView(ctrl.documentos.casoPadrao()),
+                outrosCasosView(ctrl.documentos.outrosCasos())
+            ]);
+    }
+    return m.component(require('servico/visualizar/view-vazia'));
+  }
+};
--- src/main/javascript/servico/visualizar/etapa.js
+++ src/main/javascript/servico/visualizar/etapa.js
@@ -11,34 +11,6 @@ module.exports = {
   },
 
   view: function (ctrl) {
-    var casoPadrao = function (cp) {
-      return m('ul', cp.campos().map(function (campo) {
-        return m('li', m('span', campo.descricao()));
-      }));
-    };
-
-    var outrosCasos = function (casos) {
-      return casos.map(function (caso) {
-        return m('ul.caso-descricao', [
-                m('.info-etapa', caso.descricao()),
-                caso.campos().map(function (c) {
-            return m('li', m('span', c.descricao()));
-          })
-            ]);
-      });
-    };
-
-    var documentos = function (etapa) {
-      if (!_.isEmpty(etapa.documentos())) {
-        return m('.subtitulo-etapa', [
-                m('p.titulo-documento', 'Documentação'),
-                m('p.info-etapa', 'Documentação necessária'),
-                casoPadrao(etapa.documentos().casoPadrao()),
-                outrosCasos(etapa.documentos().outrosCasos())
-        ]);
-      }
-      return m.component(require('servico/visualizar/view-vazia'));
-    };
 
     var custoPadrao = function (cp) {
       return m('ul', cp.campos().map(function (campo) {
@@ -125,7 +97,7 @@ module.exports = {
                 m('p.circle', index + 1),
                 m('h4.etapa', etapa.titulo() ? etapa.titulo() : 'acesse o serviço'),
                 m('.etapa markdown', m.trust(ctrl.converter.makeHtml(etapa.descricao()))),
-                documentos(etapa),
+                m.component(m.component(require('servico/visualizar/documentos'), etapa.documentos())),
                 custos(etapa),
                 canaisDePrestacao(etapa),
             ]);
