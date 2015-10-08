34d423fb8db3eb126ba15859fc087fd3a8e5414c
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-08 16:39:05 -0300

    Adiciona visualização de nomes populares - #75

--- src/main/javascript/servico/visualizar/outras-info.js
+++ src/main/javascript/servico/visualizar/outras-info.js
@@ -2,9 +2,23 @@
 
 module.exports = {
 
-  controller: function (args) {},
+  controller: function (args) {
+    this.servico = args;
+  },
 
-  view: function () {
-    return m('', 'Outras informações');
+  view: function (ctrl) {
+    return m('', [
+            m('h3#servico-outras-info.subtitulo-servico', 'Outras informações'),
+            m('.info-extra', [
+                !_.isEmpty(ctrl.servico.nomesPopulares()) ? m('.row',
+          m('p', ['Você também pode conhecer este serviço como: ',
+                        ctrl.servico.nomesPopulares().join(', '),
+                        '.'
+                    ])
+                ) : m(''),
+                m('div', m('p', 'Este serviço é gratuito para o cidadão.'))
+            ])
+
+        ]);
   }
 };
