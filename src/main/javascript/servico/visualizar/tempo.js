4ba009dd649822ffca4ef8531922ca9450a75827
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-08 15:11:04 -0300

    Adiciona dados para visualização de tempo e refatora views - #75

--- src/main/javascript/servico/visualizar/tempo.js
+++ src/main/javascript/servico/visualizar/tempo.js
@@ -1,19 +1,62 @@
 'use strict';
 
+var referencia = require('referencia');
+
 module.exports = {
 
   controller: function (args) {
     this.servico = args;
+    this.unidades = referencia.unidadesDeTempoVisualizar;
 
-    this.temTempo = function() {
-        return true;
+    this.temTempoEntre = function () {
+      return (!_.isEmpty(this.servico.tempoTotalEstimado().entreMinimo()) || !_.isEmpty(this.servico.tempoTotalEstimado().entreMaximo()));
+    };
+
+    this.temTempoAte = function () {
+      return !_.isEmpty(this.servico.tempoTotalEstimado().ateMaximo());
+    };
+
+    this.temTempo = function () {
+      return this.servico.tempoTotalEstimado() && (this.temTempoAte() || this.temTempoEntre());
     };
   },
 
   view: function (ctrl) {
-      if (ctrl.temTempo()) {
-          return m('#servico-tempo', []);
+
+    var tempoEntreView = function () {
+      if (ctrl.temTempoEntre()) {
+        return m('p', [
+                  'Entre ',
+                  m('span', ctrl.servico.tempoTotalEstimado().entreMinimo()),
+                  ' e ',
+                  m('span', ctrl.servico.tempoTotalEstimado().entreMaximo()),
+                  ' ',
+                  m('span', ctrl.unidades[ctrl.servico.tempoTotalEstimado().entreTipoMaximo()]),
+                  ' é o tempo estimado para a prestação imediata deste serviço.'
+              ]);
       }
       return m.component(require('servico/visualizar/view-vazia'));
+    };
+
+    var tempoAteView = function () {
+      if (ctrl.temTempoAte()) {
+        return m('p', [
+                  m('span', 'Até '),
+                  m('span', ctrl.servico.tempoTotalEstimado().ateMaximo()),
+                  m('span', ' '),
+                  m('span', ctrl.servico.tempoTotalEstimado().ateTipoMaximo()),
+              ]);
+      }
+      return m.component(require('servico/visualizar/view-vazia'));
+    };
+
+    if (ctrl.temTempo()) {
+      return m('#servico-tempo', [
+              m('h3.subtitulo-servico', 'Quanto tempo leva?'),
+              tempoEntreView(),
+                tempoAteView()
+          ]);
+    }
+    return m.component(require('servico/visualizar/view-vazia'));
   }
 };
