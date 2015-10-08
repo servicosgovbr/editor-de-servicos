5ab1b8646122cddb8ce6c321e713df5e32734682
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-08 16:13:24 -0300

    Adiciona visualização de lesgislação - #75

--- src/main/javascript/servico/visualizar/legislacao.js
+++ src/main/javascript/servico/visualizar/legislacao.js
@@ -2,9 +2,24 @@
 
 module.exports = {
 
-  controller: function (args) {},
+  controller: function (args) {
+    this.servico = args;
+    this.converter = new window.showdown.Converter();
 
-  view: function () {
-    return m('', 'Legislação');
+    this.temLegislacao = function () {
+      return !_.isEmpty(this.servico.legislacoes());
+    };
+  },
+
+  view: function (ctrl) {
+    if (ctrl.temLegislacao()) {
+      return m('#servico-legislacao', [
+              m('h3.subtitulo-servico', 'Legislação'),
+              m('ul', ctrl.servico.legislacoes().map(function (legislacao) {
+          return m('li', m('span', m.trust(ctrl.converter.makeHtml(legislacao))));
+        }))
+          ]);
+    }
+    return m.component(require('servico/visualizar/view-vazia'));
   }
 };
