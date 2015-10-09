ba309d015393eb33c8bcdd05c9ebc82ca2b7e2bd
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-09 14:28:04 -0300

    Adiciona informações de feedback - #75

--- src/main/javascript/servico/visualizar/feedback.js
+++ src/main/javascript/servico/visualizar/feedback.js
@@ -7,7 +7,35 @@ module.exports = {
   view: function () {
     return m('form.feedback-servico', [
         m('h3.opiniao', 'Você achou as informações acima úteis?'),
-        m('p.formulario-opiniao', [])
+        m('p.formulario-opiniao', [
+            m('label', {
+          for: 'sim'
+        }, 'Sim'),
+            m('input[type=radio].left', {
+          name: 'conteudoEncontrado',
+          id: 'sim',
+          value: 'true',
+          required: 'required'
+        }),
+            m('br.clear'),
+            m('label', {
+          for: 'nao'
+        }, 'Não'),
+            m('input[type=radio].left', {
+          name: 'conteudoEncontrado',
+          id: 'nao',
+          value: 'false'
+        })
+        ]),
+        m('label', {
+        for: 'opiniao'
+      }, [
+            m('h3.opiniao', 'Quais informações você sentiu falta neste serviço?'),
+            m('textarea#opiniao.row', {
+          name: 'mensagem',
+          rows: 5
+        })
+        ])
     ]);
   }
 };
