48a0aeb183aedf8f7050e2075538d3431198d756
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-09 14:13:20 -0300

    Adiciona dados básicos de feedback - #75

--- src/main/javascript/servico/visualizar/feedback.js
+++ src/main/javascript/servico/visualizar/feedback.js
@@ -5,6 +5,9 @@ module.exports = {
   controller: function (args) {},
 
   view: function () {
-    return m('', 'Feedback');
+    return m('form.feedback-servico', [
+        m('h3.opiniao', 'Você achou as informações acima úteis?'),
+        m('p.formulario-opiniao', [])
+    ]);
   }
 };
