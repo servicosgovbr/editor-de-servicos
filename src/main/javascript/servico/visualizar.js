67ded089399a6185428285e902c9d713a11ce7c6
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-09 17:51:16 -0300

    CSS inicial da visualização - #75

--- src/main/javascript/servico/visualizar.js
+++ src/main/javascript/servico/visualizar.js
@@ -14,23 +14,25 @@ module.exports = {
   view: function (ctrl, args) {
     var servico = limparModelo(ctrl.servico());
 
-    return m('#conteudo', [m('span.cabecalho-cor'),
+    return m('#conteudo', [
+                m('span.cabecalho-cor'),
                 m('#wrapper', [
                 m.component(require('cabecalho/cabecalho'), {
           metadados: false,
           logout: true,
           cabecalho: ctrl.cabecalho
         }),
-                m('#servico', m('.scroll', [m('.row', m('h2', servico.nome() + (servico.sigla() ? '|' + servico.sigla() + '|' : ''))),
-                      m('.row', m.component(require('servico/visualizar/ancoras'), servico)),
-                      m('.row', m.component(require('servico/visualizar/descricao'), servico)),
-                      m('.row', m.component(require('servico/visualizar/solicitantes'), servico)),
-                      m('.row', m.component(require('servico/visualizar/etapas'), servico)),
-                      m('.row', m.component(require('servico/visualizar/tempo'), servico)),
-                      m('.row', m.component(require('servico/visualizar/legislacao'), servico)),
-                      m('.row', m.component(require('servico/visualizar/outras-info'), servico)),
-                      m('.row', m.component(require('servico/visualizar/feedback')))
-                    ]))])
-            ]);
+                m('#visualizar', m('#servico', m('.scroll', [m('.row', m('h2', servico.nome() + (servico.sigla() ? '|' + servico.sigla() + '|' : ''))),
+                        m('.row', m.component(require('servico/visualizar/ancoras'), servico)),
+                        m('.row', m.component(require('servico/visualizar/descricao'), servico)),
+                        m('.row', m.component(require('servico/visualizar/solicitantes'), servico)),
+                        m('.row', m.component(require('servico/visualizar/etapas'), servico)),
+                        m('.row', m.component(require('servico/visualizar/tempo'), servico)),
+                        m('.row', m.component(require('servico/visualizar/legislacao'), servico)),
+                        m('.row', m.component(require('servico/visualizar/outras-info'), servico)),
+                        m('.row', m.component(require('servico/visualizar/feedback')))
+                    ])))
+                ])
+        ]);
   }
 };
