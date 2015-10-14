20be2bb28ef10f2f0ef0eba924ce1faee880720e
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-14 15:11:01 -0300

    Refatora ancoras para usarem métodos no controller - #75

--- src/main/javascript/servico/visualizar/ancoras.js
+++ src/main/javascript/servico/visualizar/ancoras.js
@@ -4,15 +4,31 @@ module.exports = {
 
   controller: function (args) {
     this.servico = args;
+
+    this.temSolicitante = function () {
+      return !_.isEmpty(this.servico.solicitantes());
+    };
+
+    this.temEtapas = function () {
+      return !_.isEmpty(this.servico.etapas());
+    };
+
+    this.temTempoEstimado = function () {
+      return !_.isUndefined(this.servico.tempoTotalEstimado()) && (!_.isUndefined(this.servico.tempoTotalEstimado().ate) || !_.isUndefined(this.servico.tempoTotalEstimado().entre));
+    };
+
+    this.temLegislacao = function () {
+      return !_.isEmpty(this.servico.legislacoes());
+    };
   },
 
   view: function (ctrl) {
     return m('.ancoras', [m('ul', [
             m('li', [m('a', 'O que é?')]),
-            !_.isEmpty(ctrl.servico.solicitantes()) ? m('li', [m('a', 'Quem pode utilizar este serviço?')]) : m(''),
-            !_.isEmpty(ctrl.servico.etapas()) ? m('li', [m('a', 'Etapas para a realização desde serviço')]) : m(''),
-            !_.isUndefined(ctrl.servico.tempoTotalEstimado()) && (!_.isUndefined(ctrl.servico.tempoTotalEstimado().ate) || !_.isUndefined(ctrl.servico.tempoTotalEstimado().entre)) ? m('li', [m('a', 'Quanto tempo leva?')]) : m(''),
-            !_.isEmpty(ctrl.servico.legislacoes()) ? m('li', [m('a', 'Legislação')]) : m(''),
+            ctrl.temSolicitante() ? m('li', [m('a', 'Quem pode utilizar este serviço?')]) : m(''),
+            ctrl.temEtapas() ? m('li', [m('a', 'Etapas para a realização desde serviço')]) : m(''),
+            ctrl.temTempoEstimado() ? m('li', [m('a', 'Quanto tempo leva?')]) : m(''),
+            ctrl.temLegislacao() ? m('li', [m('a', 'Legislação')]) : m(''),
             m('li', [m('a', 'Outras informações')])
         ])]);
   }
