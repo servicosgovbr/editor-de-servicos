fb2bf1d9ec9713d3c5288a540fce7e8a813c7353
Jean Kirchner <jeankirchner@gmail.com>
2015-11-14 18:02:30 -0200

    remove duplicações de código

--- src/test/java/br/gov/servicos/editor/conteudo/cartas/ImportarXMLCartaControllerTest.java
+++ src/test/java/br/gov/servicos/editor/conteudo/cartas/ImportarXMLCartaControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.test.util.XmlExpectationsHelper;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
