a7ec1957f58791c8f7af5e00ade6c62c72d149d5
Jean Kirchner <jeankirchner@gmail.com>
2015-11-16 11:41:37 -0200

    não executa despublicar ao cancelar operação - #393

--- src/main/javascript/componentes/menu/despublicar-confirmacao.js
+++ src/main/javascript/componentes/menu/despublicar-confirmacao.js
@@ -1,9 +1,15 @@
 'use strict';
 
+var mensagem = 'Ao despublicar o serviço, a versão que está no Portal de Serviços será perdida, e apenas a versão em edição estará disponível para ser publicada novamente. Deseja despublicar o serviço?';
+
 module.exports = function (despublicarFn) {
   return function () {
     alertify.labels.cancel = 'Cancelar';
     alertify.labels.ok = 'Despublicar';
-    alertify.confirm('Tem certeza que deseja despublicar o serviço?', despublicarFn);
+    alertify.confirm(mensagem, function(ok) {
+      if (ok) {
+       despublicarFn();
+      }
+    });
   };
 };
