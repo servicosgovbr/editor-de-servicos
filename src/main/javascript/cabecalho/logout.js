19ff38b8d085b49aaee3a32ff124914532aa51a0
Pedro Leal <pedrombl@gmail.com>
2015-11-16 09:16:58 -0200

    Simplificando a criacao do form de logout #390

--- src/main/javascript/cabecalho/logout.js
+++ src/main/javascript/cabecalho/logout.js
@@ -1,10 +1,15 @@
 'use strict';
-var protecaoCsrf = require('utils/protecao-csrf');
+var atributosCsrf = require('utils/atributos-csrf');
 
 module.exports = {
 
   view: function () {
-    return m('form#logout[action=/editar/sair][method=POST]', {config: protecaoCsrf}, [
+    return m('form#logout[action=/editar/sair][method=POST]', [
+      m('input', {
+          type: 'hidden',
+          name: atributosCsrf.name,
+          value: atributosCsrf.token
+      }),
       m('button', {
           title: 'Sair do editor (logout)'
         }, m.trust('&nbsp; Sair &nbsp;'),
