445d119797d32ff6dd4d4ea8feaa954604e775d1
gustavo-freitas <gustavof@gmail.com>
2015-11-16 19:05:11 -0200

    Adiciona novo usuário

---
+++ src/main/resources/db/migration/V2__usuario.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (username, password, enabled) VALUES ('josselmo.bezerra@planejamento.gov.br', '$2a$10$1O.BjADPpzYc2qm6c27U8ucMfZEhhHUALb/4TjiQMMbjoRgIqqizm', true);
+INSERT INTO public.authorities (username, authority) VALUES ('josselmo.bezerra@planejamento.gov.br', 'ROLE_ADMIN');
