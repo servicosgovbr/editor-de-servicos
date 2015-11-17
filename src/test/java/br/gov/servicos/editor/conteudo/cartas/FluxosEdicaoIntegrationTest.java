40c7446a1bd6a950d982411ccea2e27845b8eafa
Jean Kirchner <jeankirchner@gmail.com>
2015-11-17 10:17:23 -0200

    testes de carga para salvar cartas - #396

---
+++ src/test/java/br/gov/servicos/editor/conteudo/cartas/FluxosEdicaoIntegrationTest.java
@@ -0,0 +1,63 @@
+package br.gov.servicos.editor.conteudo.cartas;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class FluxosEdicaoIntegrationTest extends RepositorioGitIntegrationTest {
+
+    private final int OPERATION_COUNT = 37;
+
+    static String CARTA_A = "<servico><nome>Carta A</nome></servico>";
+    static String CARTA_A_ALTERACOES = "<servico><nome>Carta A</nome><sigla>CA</sigla></servico>";
+    static String CARTA_B = "<servico><nome>Carta B</nome></servico>";
+    static String CARTA_B_ALTERACOES = "<servico><nome>Carta B</nome><sigla>CB</sigla></servico>";
+
+    @Before
+    public void setup() {
+        setupBase()
+                .carta("carta-a", CARTA_A)
+                .carta("carta-b", CARTA_B)
+                .build();
+    }
+
+    @Test
+    public void testeCargaSalvar() throws Exception {
+        for (int i = 0; i < OPERATION_COUNT; i++) {
+            api.salvarCarta("carta-a", CARTA_A)
+                    .andExpect(status().is3xxRedirection());
+            api.editarCarta("carta-a")
+                    .andExpect(status().isOk())
+                    .andExpect(content().xml(CARTA_A));
+            api.salvarCarta("carta-a", CARTA_A_ALTERACOES)
+                    .andExpect(status().is3xxRedirection());
+            api.editarCarta("carta-a")
+                    .andExpect(status().isOk())
+                    .andExpect(content().xml(CARTA_A_ALTERACOES));
+            api.salvarCarta("carta-b", CARTA_B)
+                    .andExpect(status().is3xxRedirection());
+            api.editarCarta("carta-b")
+                    .andExpect(status().isOk())
+                    .andExpect(content().xml(CARTA_B));
+            api.salvarCarta("carta-b", CARTA_B_ALTERACOES)
+                    .andExpect(status().is3xxRedirection());
+            api.editarCarta("carta-b")
+                    .andExpect(status().isOk())
+                    .andExpect(content().xml(CARTA_B_ALTERACOES));
+        }
+        api.descartarCarta("carta-a")
+                .andExpect(status().is3xxRedirection());
+        api.editarCarta("carta-a")
+                .andExpect(status().isOk())
+                .andExpect(content().xml(CARTA_A));
+
+        api.descartarCarta("carta-b")
+                .andExpect(status().is3xxRedirection());
+        api.editarCarta("carta-b")
+                .andExpect(status().isOk())
+                .andExpect(content().xml(CARTA_B));
+    }
+
+}
--- src/test/java/br/gov/servicos/editor/fixtures/MockMvcFactory.java
+++ src/test/java/br/gov/servicos/editor/fixtures/MockMvcFactory.java
@@ -1,7 +1,6 @@
 package br.gov.servicos.editor.fixtures;
 
 import br.gov.servicos.editor.conteudo.MockMvcEditorAPI;
-import br.gov.servicos.editor.conteudo.MockMvcEditorAsyncAPI;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -16,11 +15,6 @@ public class MockMvcFactory {
 
     public static MockMvcEditorAPI editorAPI(WebApplicationContext context) {
         return new MockMvcEditorAPI(get(context));
-    }
-
-    public static MockMvcEditorAsyncAPI editorAsynAPI(WebApplicationContext context) {
-        MockMvc mvc = get(context);
-        return new MockMvcEditorAsyncAPI(mvc, new MockMvcEditorAPI(mvc));
     }
 
 }
