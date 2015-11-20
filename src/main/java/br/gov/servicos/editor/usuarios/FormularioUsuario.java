1d8c15125ad0097839132ad9e15e8d08728d1cc8
Pedro Leal <pedrombl@gmail.com>
2015-11-20 11:15:06 -0200

    Valida formato de CPF no cadastro #405

--- src/main/java/br/gov/servicos/editor/usuarios/FormularioUsuario.java
+++ src/main/java/br/gov/servicos/editor/usuarios/FormularioUsuario.java
@@ -1,10 +1,12 @@
 package br.gov.servicos.editor.usuarios;
 
 
+import br.com.caelum.stella.bean.validation.CPF;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.Wither;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.Size;
 
@@ -19,18 +21,29 @@ import static lombok.AccessLevel.PRIVATE;
 @EqualsAndHashCode
 public class FormularioUsuario {
     public static final String EMAIL_INVALIDO = "formato de email não valido";
+    public static final String CAMPO_OBRIGATORIO = "campo obrigatório";
+
+    @NotBlank(message = "CPF: " + CAMPO_OBRIGATORIO)
+    @CPF(message = "CPF: não é valido")
     @CpfUnico
     String cpf;
+
     @Size(min=8, max=50, message = "Senha: tamanho deve estar entre 8 e 50")
     String senha;
+
     String papelId;
+
     String siorg;
+
     @SiapeUnico
     String siape;
+
     @Email(message = "Email Institucional: " + EMAIL_INVALIDO)
     String emailInstitucional;
+
     @Email(message = "Email Secundario: " + EMAIL_INVALIDO)
     String emailSecundario;
+
     boolean servidor;
 
 }
