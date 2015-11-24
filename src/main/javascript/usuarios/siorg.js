8ac2b49d5429566aa1340f32c6c56d1a39f55868
Glauber Ramos <glauberamos@gmail.com>
2015-11-24 14:03:52 -0200

    Mostra erro para senhas #405

--- src/main/javascript/usuarios/siorg.js
+++ src/main/javascript/usuarios/siorg.js
@@ -7,15 +7,15 @@ module.exports = function (m, siorgInicial, element) {
   siorg.nome = m.prop(siorgInicial);
   siorg.view = function () {
     return m('label', [
-            m('h3', 'Orgão'),
-            m.component(selectOrgao, {
-        prop: siorg.nome
-      }),
-            m('input[type=hidden]#siorg', {
-        name: 'siorg',
-        value: siorg.nome()
-      })
-        ]);
+        m('h3', 'Orgão'),
+        m.component(selectOrgao, {
+          prop: siorg.nome
+        }),
+        m('input[type=hidden]#siorg', {
+          name: 'siorg',
+          value: siorg.nome()
+        })
+    ]);
   };
 
   m.mount(document.getElementById(element), {
