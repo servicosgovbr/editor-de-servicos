15d187e86b5b906361e35f0eac4f48830f4ef848
Glauber Ramos <glauberamos@gmail.com>
2015-12-01 16:42:38 -0200

    Desabilita botão de visualizar se está criando serviço novo e ainda não salvou #461

--- src/main/javascript/cabecalho/visualizar-button.js
+++ src/main/javascript/cabecalho/visualizar-button.js
@@ -11,9 +11,16 @@ module.exports = {
     };
   },
 
-  view: function (ctrl) {
+  view: function (ctrl, args) {
+    var meta = args.metadados;
+
+    function temEdicao() {
+      return _.get(meta, 'editado.revisao');
+    }
+
     return m('button#visualizar', {
       onclick: _.bind(ctrl.onClick, ctrl),
+      disabled: _.contains('/editar/servico/novo', m.route()) && !temEdicao()
     }, [
       m('i.fa.fa-eye'),
       m.trust('&nbsp; Visualizar')
