ebf5f7b9bd094064bd2c9cfd80061b596f477080
Jean Kirchner <jeankirchner@gmail.com>
2015-12-08 07:29:14 -0200

    script para atualizar lista do siorg

--- src/main/resources/atualiza-estrutura-organizacional.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -sH 'Accept-encoding: gzip' "http://estruturaorganizacional.dados.gov.br/doc/estrutura-organizacional/resumida.json?retornarOrgaoEntidadeVinculados;=SIM" > estrutura-organizacional.json.gz
