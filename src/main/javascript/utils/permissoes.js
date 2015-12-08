33f904b1b69530b4127627ee720219a36fd5085c
Glauber Ramos <glauberamos@gmail.com>
2015-12-08 18:41:49 -0200

    Atualiza permissões no javascript

--- src/main/javascript/utils/permissoes.js
+++ src/main/javascript/utils/permissoes.js
@@ -11,10 +11,12 @@ function retornaPermissoes() {
 
 var arrayPermissoes = retornaPermissoes();
 
+//funções apoio
 function possuiPermissao(permissao) {
   return _.contains(arrayPermissoes, permissao);
 }
 
+//funções checa URL
 function estaNaPaginaServico() {
   return location.pathname.indexOf('editar/servico') > -1;
 }
@@ -27,22 +29,67 @@ function estaNaPaginaTematica() {
   return location.pathname.indexOf('editar/pagina-tematica') > -1;
 }
 
-function  podeSalvarServico() {
-  return possuiPermissao('editar_salvar SERVICO') && estaNaPaginaServico();
+//serviço
+function possuiPermissaoPaginaServico(permissao) {
+  return possuiPermissao(permissao) && estaNaPaginaServico();
 }
 
-function  podeSalvarOrgao() {
-  return possuiPermissao('editar_salvar ORGAO (ORGAO ESPECIFICO)') && estaNaPaginaOrgao();
+function possuiPermissaoPaginaServicoOrgaoEspecifico(permissao, orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissao(permissao) && estaNaPaginaServico() && orgaoIdUsuario === orgaoIdPagina;
 }
 
-function  podeSalvarPaginaTematica() {
-  return possuiPermissao('editar_salvar PAGINA_TEMATICA') && estaNaPaginaTematica();
+//orgão
+function possuiPermissaoPaginaOrgao(permissao, orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissao(permissao) && estaNaPaginaOrgao() && orgaoIdUsuario === orgaoIdPagina;
 }
 
-function podeSalvarPagina() {
-  return  podeSalvarServico() || podeSalvarPaginaTematica() || podeSalvarOrgao();
+//página temática
+function possuiPermissaoPaginaTematica(permissao) {
+  return possuiPermissao(permissao) && estaNaPaginaTematica();
+}
+
+//funções genéricas
+function podeSalvarPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return  possuiPermissaoPaginaServico('editar_salvar SERVICO') || 
+          possuiPermissaoPaginaTematica('editar_salvar PAGINA_TEMATICA') || 
+          possuiPermissaoPaginaOrgao('editar_salvar ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
+}
+
+function podeCriarPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissaoPaginaServico('criar SERVICO') || 
+         possuiPermissaoPaginaTematica('criar PAGINA-TEMATICA') || 
+         possuiPermissaoPaginaOrgao('criar ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
+}
+
+function podePublicarPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissaoPaginaServicoOrgaoEspecifico('publicar SERVICO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina) || 
+         possuiPermissaoPaginaTematica('publicar PAGINA_TEMATICA') || 
+         possuiPermissaoPaginaOrgao('publicar ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
+}
+
+function podeDescartarPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissaoPaginaServicoOrgaoEspecifico('descartar SERVICO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina) || 
+         possuiPermissaoPaginaTematica('descartar PAGINA_TEMATICA') || 
+         possuiPermissaoPaginaOrgao('descartar ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
+}
+
+function podeDespublicarPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissaoPaginaServicoOrgaoEspecifico('despublicar SERVICO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina) || 
+         possuiPermissaoPaginaTematica('despublicar PAGINA_TEMATICA') || 
+         possuiPermissaoPaginaOrgao('despublicar ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
+}
+
+function podeExcluirPagina(orgaoIdUsuario, orgaoIdPagina) {
+  return possuiPermissaoPaginaServicoOrgaoEspecifico('excluir SERVICO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina) || 
+         possuiPermissaoPaginaTematica('excluir PAGINA_TEMATICA') || 
+         possuiPermissaoPaginaOrgao('excluir ORGAO (ORGAO ESPECIFICO)', orgaoIdUsuario, orgaoIdPagina);
 }
 
 module.exports = {
-  podeSalvarPagina: podeSalvarPagina
+  podeSalvarPagina: podeSalvarPagina,
+  podeCriarPagina: podeCriarPagina,
+  podePublicarPagina: podePublicarPagina,
+  podeDespublicarPagina: podeDespublicarPagina,
+  podeDescartarPagina: podeDescartarPagina,
+  podeExcluirPagina: podeExcluirPagina
 };
