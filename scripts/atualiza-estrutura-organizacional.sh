cb64b4eb0b379435c572af952c88b3a6d20a09ac
Jean Kirchner <jeankirchner@gmail.com>
2015-12-08 07:43:46 -0200

    melhora o script de atualização da estrutura organizacional do SIORG

--- scripts/atualiza-estrutura-organizacional.sh
+++ scripts/atualiza-estrutura-organizacional.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+git add . -A
+git stash
+git pull --rebase
+
+
+echo 'Iniciando download lista de órgãos do SIORG'
 curl -sH 'Accept-encoding: gzip' "http://estruturaorganizacional.dados.gov.br/doc/estrutura-organizacional/resumida.json?retornarOrgaoEntidadeVinculados;=SIM" > src/main/resources/estrutura-organizacional.json.gz
+echo 'Lista de órgãos do SIORG foi atualizada!'
+
+
+git pull --rebase
+git push
+git stash pop
+
