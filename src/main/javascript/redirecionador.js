056eff63c4ea2c230745502bcb8a2547a4029626
André Leite <andre@andre.io>
2015-12-21 17:49:08 -0200

    Ajustando redirecionamento na criação e edição de páginas temáticas

--- src/main/javascript/redirecionador.js
+++ src/main/javascript/redirecionador.js
@@ -4,7 +4,7 @@ var slugify = require('slugify');
 
 module.exports = function (tipo, nome) {
   var oldId = m.route.param('id');
-  var newId = slugify(nome);
+  var newId = slugify(nome) || oldId;
   if (!_.isEqual(oldId, newId)) {
     m.route('/editar/' + tipo + '/' + newId);
   }
