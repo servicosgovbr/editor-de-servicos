f30ba1fd934b40094b1d421901c778d21aa10c5d
isaacvits <vitortannus@gmail.com>
2016-05-03 15:18:41 -0300

    Carregando o Branch List de todo o jeito

--- src/main/java/br/gov/servicos/editor/git/RepositorioGit.java
+++ src/main/java/br/gov/servicos/editor/git/RepositorioGit.java
@@ -194,16 +194,10 @@ public class RepositorioGit {
 
             if (repository.getRef(novoBranch) == null) {
 
-                List<Ref> remoteBranches;
-                if (ListaDeConteudo.CacheEsquentando.get()) {
-                    remoteBranches = singletonList(repository.getRef(branchRemoto));
-                } else {
-                    remoteBranches = git.branchList()
+                List<Ref> remoteBranches = git.branchList()
                             .setListMode(REMOTE)
                             .call();
-                }
-                
-
+            
                 if (remoteBranches.contains(repository.getRef(branchRemoto))) {
                     checkoutNovoBranch(novoBranch, branchRemoto);
                 } else {
